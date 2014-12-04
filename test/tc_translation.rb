13a378c1cc29560cd3773bc5dbf6954cd29fb2e0
Josh Bradley <jbradley@arcticlcc.org>
2014-12-04 14:58:08 -0900

    Add test for 19110.

--- test/tc_translation.rb
+++ test/tc_translation.rb
@@ -8,49 +8,66 @@ require 'minitest/autorun'
 require 'json'
 require File.join(File.expand_path('..', __FILE__),'..','lib', 'adiwg-mdtranslator.rb')
 
-class TestTranslation_v0_8 < MiniTest::Unit::TestCase
-  @@reader = 'adiwgJson'
+class TestTranslation_v0_9 < MiniTest::Test
+  @@reader = 'mdJson'
   @@writer = 'iso19115_2'
 
-  def test_ouput_success
-    # read test adiwg full json test
-    file = File.open(File.join(File.dirname(__FILE__),'schemas','v0_8','examples','full_example.json'), 'r')
+  def test_full_success
+    # read mdJson full
+    file = File.open(File.join(File.dirname(__FILE__),'schemas','v0_9','examples','full_example.json'), 'r')
     jsonObj = file.read
     file.close
 
     # call opening module in mdTranslator
-
-    metadata = ADIWG::Mdtranslator.translate(jsonObj,@@reader,@@writer,'normal','true')
+    metadata = ADIWG::Mdtranslator.translate(jsonObj, @@reader,@@writer,'normal','true')
     version = JSON.parse(jsonObj)['version']['version'].split('.')
 
-    assert_equal('json',metadata[:readerFormat])
-    assert metadata[:readerStructurePass]
-    assert_equal(@@reader,metadata[:readerName])
+    assert_equal('json',metadata[:readerFormat], 'Check reader name')
+    assert metadata[:readerStructurePass], metadata[:readerStructureMessages].join(',')
+    assert_equal(@@reader,metadata[:readerFound])
     #major version
     assert_equal(version[0],metadata[:readerVersionUsed].split('.')[0])
     #minor version
     assert_equal(version[1],metadata[:readerVersionUsed].split('.')[1])
-    assert metadata[:readerValidationPass]
+    assert metadata[:readerValidationPass], "reader validation failed: \n" + metadata[:readerValidationMessages].join(',')
     assert_equal(@@writer,metadata[:writerName])
-    assert metadata[:writerPass]
+    assert metadata[:writerPass], "writer validation failed: \n" + metadata[:writerMessages].join(',')
     refute_nil metadata[:writerOutput]
   end
 
-  def test_minimum
-    # read test adiwg minimum json test
-    file = File.open(File.join(File.dirname(__FILE__),'schemas','v0_8','examples','minimum_example.json'), 'r')
+  def test_minimum_success
+    # read mdJson minimum
+    file = File.open(File.join(File.dirname(__FILE__),'schemas','v0_9','examples','minimum_example.json'), 'r')
     jsonObj = file.read
     file.close
 
     # call opening module in mdTranslator
     metadata = ADIWG::Mdtranslator.translate(jsonObj,@@reader,@@writer,'normal','true')
 
-    assert_equal('json',metadata[:readerFormat])
-    assert metadata[:readerStructurePass]
-    assert_equal(@@reader,metadata[:readerName])
-    assert metadata[:readerValidationPass]
+    assert_equal('json',metadata[:readerFormat], 'Check reader name')
+    assert metadata[:readerStructurePass], metadata[:readerStructureMessages].join(',')
+    assert_equal(@@reader,metadata[:readerFound])
+    assert metadata[:readerValidationPass], "reader validation failed: \n" + metadata[:readerValidationMessages].join(',')
     assert_equal(@@writer,metadata[:writerName])
-    assert metadata[:writerPass]
+    assert metadata[:writerPass], "writer validation failed: \n" + metadata[:writerMessages].join(',')
+    refute_nil metadata[:writerOutput]
+  end
+
+  def test_19110_success
+    # read mdJson full
+    file = File.open(File.join(File.dirname(__FILE__),'schemas','v0_9','examples','full_example.json'), 'r')
+    jsonObj = file.read
+    file.close
+
+    # call opening module in mdTranslator
+    metadata = ADIWG::Mdtranslator.translate(jsonObj,@@reader,'iso19110','normal','true')
+
+    assert_equal('json',metadata[:readerFormat], 'Check reader name')
+    assert metadata[:readerStructurePass], metadata[:readerStructureMessages].join(',')
+    assert_equal(@@reader,metadata[:readerFound])
+    assert metadata[:readerValidationPass], "reader validation failed: \n" + metadata[:readerValidationMessages].join(',')
+    assert_equal('iso19110',metadata[:writerName])
+    assert metadata[:writerPass], "writer validation failed: \n" + metadata[:writerMessages].join(',')
     refute_nil metadata[:writerOutput]
   end
 end
