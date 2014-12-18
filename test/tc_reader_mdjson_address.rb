8c233483080176d223bc2c03b81d0d0e41c3776b
stansmith907 <stansmith@usgs.gov>
2014-12-17 17:15:04 -0900

    - test case address using schema/v0_9/examples

--- test/tc_reader_mdjson_address.rb
+++ test/tc_reader_mdjson_address.rb
@@ -4,34 +4,33 @@
 # History:
 # Stan Smith 2014-12-09 original script
 # Stan Smith 2014-12-15 modified to use namespaces added to mdTranslator
+# Stan Smith 2014-12-16 restructure tests to use schema examples
 
 require 'minitest/autorun'
 require 'json'
 require 'adiwg/mdtranslator/internal/internal_metadata_obj'
 require 'adiwg/mdtranslator/readers/mdJson/modules_0.9/module_address'
 
-
 class TestReaderMdJsonAddress_v0_9 < MiniTest::Test
+
+    # get json test example
+    file = File.open('test/schemas/v0_9/examples/address.json', 'r')
+    jsonFile = file.read
+    file.close
+    aIn = JSON.parse(jsonFile)
+    @@hIn = aIn[0]
 
     def test_build_full_address_object
 
-        json_string = '{ ' +
-            '"deliveryPoint": ["Line 1","2 Line"],' +
-            '"city": "City",' +
-            '"administrativeArea": "ST",' +
-            '"postalCode": "99999-9999",' +
-            '"country": "US",' +
-            '"electronicMailAddress": ["hello@example.com","bye@example.com"]' +
-            '}'
-        hIn = JSON.parse(json_string)
+        hIn = @@hIn
 
         intObj = {
-            deliveryPoints: ['Line 1', '2 Line'],
-            city: 'City',
-            adminArea: 'ST',
-            postalCode: '99999-9999',
-            country: 'US',
-            eMailList: %w[hello@example.com bye@example.com]
+            deliveryPoints: %w[deliveryPoint1 deliveryPoint2],
+            city: 'city',
+            adminArea: 'administrativeArea',
+            postalCode: 'postalCode',
+            country: 'country',
+            eMailList: %w[example1@example.com example2@example.com]
         }
 
         assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Address.unpack(hIn)
@@ -40,10 +39,12 @@ class TestReaderMdJsonAddress_v0_9 < MiniTest::Test
 
     def test_email_address_only
 
-        json_string = '{ ' +
-            '"electronicMailAddress": ["hello@example.com"]' +
-            '}'
-        hIn = JSON.parse(json_string)
+        hIn = @@hIn
+        hIn.delete('deliveryPoint')
+        hIn.delete('city')
+        hIn.delete('administrativeArea')
+        hIn.delete('postalCode')
+        hIn.delete('country')
 
         intObj = {
             deliveryPoints: [],
@@ -51,7 +52,7 @@ class TestReaderMdJsonAddress_v0_9 < MiniTest::Test
             adminArea: nil,
             postalCode: nil,
             country: nil,
-            eMailList: %w[hello@example.com]
+            eMailList: %w[example1@example.com example2@example.com]
         }
 
         assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Address.unpack(hIn)
@@ -60,13 +61,11 @@ class TestReaderMdJsonAddress_v0_9 < MiniTest::Test
 
     def test_build_empty_address_object
 
-        json_string = '{}'
-        hIn = JSON.parse(json_string)
+        hIn = JSON.parse('{}')
         intObj = nil
 
         assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Address.unpack(hIn)
 
     end
-
 
 end
