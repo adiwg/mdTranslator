2ad1780f0f9a0aded4cdfd85dca6267bc30489b2
Stan Smith <adtech@alaska.net>
2015-01-19 19:09:03 -0900

    - refactored phone test to use contact.json example

--- test/readers/mdJson/v0_9/tc_reader_mdjson_phone.rb
+++ test/readers/mdJson/v0_9/tc_reader_mdjson_phone.rb
@@ -10,118 +10,100 @@ require 'json'
 require 'adiwg/mdtranslator/internal/internal_metadata_obj'
 require 'adiwg/mdtranslator/readers/mdJson/modules_0.9/module_phone'
 
-class TestReaderMdJsonAddress_v0_9 < MiniTest::Test
+class TestReaderMdJsonPhone_v0_9 < MiniTest::Test
 
-    def test_build_full_phone_object
+    # get json test example
+    file = File.open('test/schemas/v0_9/examples/contact.json', 'r')
+    jsonFile = file.read
+    file.close
+    aIn = JSON.parse(jsonFile)
+    @@hIn = aIn[0]['phoneBook'][0]
 
-        json_string = '{ ' +
-            '"phoneName": "Name",' +
-            '"phoneNumber": "(999)999-9999",' +
-            '"service": ["voice","fax","sms"]' +
-            '}'
-        hIn = JSON.parse(json_string)
+    # set namespace
+    @@NameSpace = ADIWG::Mdtranslator::Readers::MdJson::Phone
 
+    def test_complete_phone_object
+
+
+        hIn = @@hIn.clone
         intObj = []
-        intObjPart = {
-            phoneServiceType: 'voice',
-            phoneName: 'Name',
-            phoneNumber: '(999)999-9999'
+        intObj << {
+            phoneServiceType: 'service11',
+            phoneName: 'phoneName1',
+            phoneNumber: '111-111-1111'
         }
-        intObj << intObjPart
-        intObjPart = {
-            phoneServiceType: 'fax',
-            phoneName: 'Name',
-            phoneNumber: '(999)999-9999'
+        intObj << {
+            phoneServiceType: 'service12',
+            phoneName: 'phoneName1',
+            phoneNumber: '111-111-1111'
         }
-        intObj << intObjPart
-        intObjPart = {
-            phoneServiceType: 'sms',
-            phoneName: 'Name',
-            phoneNumber: '(999)999-9999'
-        }
-        intObj << intObjPart
 
-        assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Phone.unpack(hIn)
+        assert_equal intObj, @@NameSpace.unpack(hIn)
 
     end
 
     def test_missing_phone_service
 
-        json_string = '{ ' +
-            '"phoneName": "Name",' +
-            '"phoneNumber": "(999)999-9999"' +
-            '}'
-        hIn = JSON.parse(json_string)
+        hIn = @@hIn.clone
+        hIn.delete('service')
 
         intObj = []
-        intObjPart = {
+        intObj << {
             phoneServiceType: 'voice',
-            phoneName: 'Name',
-            phoneNumber: '(999)999-9999'
+            phoneName: 'phoneName1',
+            phoneNumber: '111-111-1111'
         }
-        intObj << intObjPart
 
-        assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Phone.unpack(hIn)
+        assert_equal intObj, @@NameSpace.unpack(hIn)
 
     end
 
-    def test_null_phone_service
+    def test_empty_phone_service
 
-        json_string = '{ ' +
-            '"phoneName": "Name",' +
-            '"phoneNumber": "(999)999-9999",' +
-            '"service": ""' +
-            '}'
-        hIn = JSON.parse(json_string)
+        hIn = @@hIn.clone
+        hIn['service'] = ''
 
         intObj = []
-        intObjPart = {
+        intObj << {
             phoneServiceType: 'voice',
-            phoneName: 'Name',
-            phoneNumber: '(999)999-9999'
+            phoneName: 'phoneName1',
+            phoneNumber: '111-111-1111'
         }
-        intObj << intObjPart
 
-        assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Phone.unpack(hIn)
+        assert_equal intObj, @@NameSpace.unpack(hIn)
 
     end
 
     def test_missing_phone_number
 
-        json_string = '{ ' +
-            '"service": ["service"]' +
-            '}'
-        hIn = JSON.parse(json_string)
-
-        intObj = []
-
-        assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Phone.unpack(hIn)
-
-    end
-
-    def test_null_phone_number
-
         # missing phone number should return empty object
-        json_string = '{ ' +
-            '"phoneName": "",' +
-            '"phoneNumber": "",' +
-            '"service": ""' +
-            '}'
-        hIn = JSON.parse(json_string)
+        hIn = @@hIn.clone
+        hIn.delete('phoneNumber')
 
         intObj = []
 
-        assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Phone.unpack(hIn)
+        assert_equal intObj, @@NameSpace.unpack(hIn)
 
     end
 
-    def test_build_empty_phone_object
+    def test_empty_phone_number
 
-        json_string = '{}'
-        hIn = JSON.parse(json_string)
+        # empty phone number should return empty object
+        hIn = @@hIn.clone
+        hIn['phoneNumber'] = ''
+
         intObj = []
 
-        assert_equal intObj, ADIWG::Mdtranslator::Readers::MdJson::Phone.unpack(hIn)
+        assert_equal intObj, @@NameSpace.unpack(hIn)
+
+    end
+
+    def test_empty_phone_object
+
+        hIn = {}
+        intObj = []
+
+        assert_equal intObj, @@NameSpace.unpack(hIn)
 
     end
 
