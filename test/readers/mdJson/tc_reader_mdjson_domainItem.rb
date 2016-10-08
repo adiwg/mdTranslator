aff593f2f08a041df23495ff9ec09edaa083d86d
Stan Smith <adtech@alaska.net>
2016-10-07 16:18:01 -0800

    Write mdJson 2.0 reader module for 'domainItem' Include minitest module

---
+++ test/readers/mdJson/tc_reader_mdjson_domainItem.rb
@@ -0,0 +1,98 @@
+# MdTranslator - minitest of
+# reader / mdJson / module_domainMember
+
+# History:
+#   Stan Smith 2016-10-07 refactored for mdJson 2.0
+#   Stan Smith 2015-07-23 original script
+
+require 'minitest/autorun'
+require 'json'
+require 'adiwg/mdtranslator/internal/internal_metadata_obj'
+require 'adiwg/mdtranslator/readers/mdJson/mdJson_reader'
+require 'adiwg/mdtranslator/readers/mdJson/modules/module_domainItem'
+
+class TestReaderMdJsonDomainItem_v1 < MiniTest::Test
+
+    # set constants and variables
+    @@NameSpace = ADIWG::Mdtranslator::Readers::MdJson::DomainItem
+    @@responseObj = {
+        readerExecutionMessages: [],
+        readerExecutionPass: true
+    }
+
+    # get json file for tests from examples folder
+    file = File.join(File.dirname(__FILE__), '../../', 'schemas/examples', 'domainItem.json')
+    file = File.open(file, 'r')
+    jsonFile = file.read
+    file.close
+    aIn = JSON.parse(jsonFile)
+
+    # only the first instance in the example array is used for tests
+    # the first example is fully populated
+    @@hIn = aIn['domainItem'][0]
+
+    def test_complete_domainItem_object
+
+        hIn = Marshal::load(Marshal.dump(@@hIn))
+        hResponse = Marshal::load(Marshal.dump(@@responseObj))
+        metadata = @@NameSpace.unpack(hIn, hResponse)
+
+        assert_equal 'name', metadata[:itemName]
+        assert_equal 'value', metadata[:itemValue]
+        assert_equal 'definition', metadata[:itemDefinition]
+        assert hResponse[:readerExecutionPass]
+        assert_empty hResponse[:readerExecutionMessages]
+
+    end
+
+    def test_empty_domainItem_name
+
+        hIn = Marshal::load(Marshal.dump(@@hIn))
+        hIn['name'] = ''
+        hResponse = Marshal::load(Marshal.dump(@@responseObj))
+        metadata = @@NameSpace.unpack(hIn, hResponse)
+
+        assert_nil metadata
+        refute hResponse[:readerExecutionPass]
+        refute_empty hResponse[:readerExecutionMessages]
+
+    end
+
+    def test_empty_domainItem_value
+
+        hIn = Marshal::load(Marshal.dump(@@hIn))
+        hIn['value'] = ''
+        hResponse = Marshal::load(Marshal.dump(@@responseObj))
+        metadata = @@NameSpace.unpack(hIn, hResponse)
+
+        assert_nil metadata
+        refute hResponse[:readerExecutionPass]
+        refute_empty hResponse[:readerExecutionMessages]
+
+    end
+
+    def test_empty_domainItem_definition
+
+        hIn = Marshal::load(Marshal.dump(@@hIn))
+        hIn['definition'] = ''
+        hResponse = Marshal::load(Marshal.dump(@@responseObj))
+        metadata = @@NameSpace.unpack(hIn, hResponse)
+
+        assert_nil metadata
+        refute hResponse[:readerExecutionPass]
+        refute_empty hResponse[:readerExecutionMessages]
+
+    end
+
+    def test_empty_domainItem_object
+
+        hResponse = Marshal::load(Marshal.dump(@@responseObj))
+        metadata = @@NameSpace.unpack({}, hResponse)
+
+        assert_nil metadata
+        refute hResponse[:readerExecutionPass]
+        refute_empty hResponse[:readerExecutionMessages]
+
+    end
+
+end
