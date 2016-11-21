85ec83ac3c67b310599b5b0a28a51c26afaf9fb1
stansmith907 <swsak2hi@gmail.com>
2016-11-21 14:13:54 -0900

    Write mdTranslator 2.0 19115-2 write for 'class_codelist'

--- lib/adiwg/mdtranslator/writers/iso19115_2/classes/class_codelist.rb
+++ lib/adiwg/mdtranslator/writers/iso19115_2/classes/class_codelist.rb
@@ -2,14 +2,15 @@
 
 # from http://mdtranslator.adiwg.org/api/codelists?format=xml
 # History:
-# 	Stan Smith 2013-08-09 original script
-#   Stan Smith 2014-10-15 allow non-ISO codesNames to be rendered
-#   Stan Smith 2014-12-12 refactored to handle namespacing readers and writers
-#   Stan Smith 2014-12-15 replaced NOAA CT_CodelistCatalogue with mdTranslator CT_CodelistCatalogue
-#   Stan Smith 2015-06-11 refactored to use mdCodes gem for codelist contents
-#   Stan Smith 2015-06-22 replace global ($response) with passed in object (responseObj)
-#   Stan Smith 2015-07-14 refactored to make iso19110 independent of iso19115_2 classes
+#   Stan Smith 2016-11-19 refactored for mdTranslator/mdJson 2.0
 #   Stan Smith 2015-07-14 refactored to eliminate namespace globals $WriterNS and $IsoNS
+#   Stan Smith 2015-07-14 refactored to make iso19110 independent of iso19115_2 classes
+#   Stan Smith 2015-06-22 replace global ($response) with passed in object (responseObj)
+#   Stan Smith 2015-06-11 refactored to use mdCodes gem for codelist contents
+#   Stan Smith 2014-12-15 replaced NOAA CT_CodelistCatalogue with mdTranslator CT_CodelistCatalogue
+#   Stan Smith 2014-12-12 refactored to handle namespacing readers and writers
+#   Stan Smith 2014-10-15 allow non-ISO codesNames to be rendered
+# 	Stan Smith 2013-08-09 original script
 
 require 'adiwg-mdcodes'
 
@@ -32,7 +33,7 @@ module ADIWG
 
                         sourceName = mdCodelist['sourceName']
                         codelist = mdCodelist['codelist']
-                        codeId = 'user-provided-code'
+                        codeId = 'userCode'
 
                         # search the codelist for a matching codeName
                         codelist.each do |code|
