80946b5bc2f9f73552e6e42ff62f1245a609ccfd
stansmith907 <swsak2hi@gmail.com>
2017-02-01 18:05:54 -0900

    Write ISO 19110 writer tests for class 'onlineResource'

--- test/writers/iso19110/tc_19110_onlineResource.rb
+++ test/writers/iso19110/tc_19110_onlineResource.rb
@@ -1,5 +1,5 @@
 # MdTranslator - minitest of
-# writers / iso19110 / class_phone
+# writers / iso19110 / class_onlineResource
 
 # History:
 #   Stan Smith 2017-02-01 original script
@@ -10,26 +10,26 @@ require 'rexml/document'
 require 'adiwg/mdtranslator'
 include REXML
 
-class TestWriter19110Phone < MiniTest::Test
+class TestWriter19110OnlineResource < MiniTest::Test
 
     # read the ISO 19110 reference file
-    fname = File.join(File.dirname(__FILE__), 'resultXML', '19110_phone.xml')
+    fname = File.join(File.dirname(__FILE__), 'resultXML', '19110_onlineResource.xml')
     file = File.new(fname)
     iso_xml = Document.new(file)
     @@aRefXML = []
-    XPath.each(iso_xml, '//gmd:phone') {|e| @@aRefXML << e}
+    XPath.each(iso_xml, '//gmd:onlineResource') {|e| @@aRefXML << e}
 
     # read the mdJson 2.0 file
-    fname = File.join(File.dirname(__FILE__), 'testData', '19110_phone.json')
+    fname = File.join(File.dirname(__FILE__), 'testData', '19110_onlineResource.json')
     file = File.open(fname, 'r')
     @@mdJson = file.read
     file.close
 
-    def test_19110_phone_single
+    def test_19110_onlineResource_single
 
         hJson = JSON.parse(@@mdJson)
-        hJson['dataDictionary'][0]['responsibleParty']['party'].delete_at(1)
-        hJson['dataDictionary'][0]['responsibleParty']['party'].delete_at(1)
+        hJson['contact'][0]['onlineResource'].delete_at(1)
+        hJson['contact'][0]['onlineResource'].delete_at(1)
         jsonIn = hJson.to_json
 
         hResponseObj = ADIWG::Mdtranslator.translate(
@@ -39,17 +39,17 @@ class TestWriter19110Phone < MiniTest::Test
         metadata = hResponseObj[:writerOutput]
         iso_out = Document.new(metadata)
 
-        checkXML = XPath.first(iso_out, '//gmd:phone')
+        checkXML = XPath.first(iso_out, '//gmd:onlineResource')
 
         assert_equal @@aRefXML[0].to_s.squeeze, checkXML.to_s.squeeze
 
     end
 
-    def test_19110_phone_multiple
+    def test_19110_onlineResource_empty_elements
 
         hJson = JSON.parse(@@mdJson)
-        hJson['dataDictionary'][0]['responsibleParty']['party'].delete_at(0)
-        hJson['dataDictionary'][0]['responsibleParty']['party'].delete_at(1)
+        hJson['contact'][0]['onlineResource'].delete_at(0)
+        hJson['contact'][0]['onlineResource'].delete_at(1)
         jsonIn = hJson.to_json
 
         hResponseObj = ADIWG::Mdtranslator.translate(
@@ -59,17 +59,17 @@ class TestWriter19110Phone < MiniTest::Test
         metadata = hResponseObj[:writerOutput]
         iso_out = Document.new(metadata)
 
-        checkXML = XPath.first(iso_out, '//gmd:phone')
+        checkXML = XPath.first(iso_out, '//gmd:onlineResource')
 
         assert_equal @@aRefXML[1].to_s.squeeze, checkXML.to_s.squeeze
 
     end
 
-    def test_19110_phone_unknown
+    def test_19110_onlineResource_missing_elements
 
         hJson = JSON.parse(@@mdJson)
-        hJson['dataDictionary'][0]['responsibleParty']['party'].delete_at(0)
-        hJson['dataDictionary'][0]['responsibleParty']['party'].delete_at(0)
+        hJson['contact'][0]['onlineResource'].delete_at(0)
+        hJson['contact'][0]['onlineResource'].delete_at(0)
         jsonIn = hJson.to_json
 
         hResponseObj = ADIWG::Mdtranslator.translate(
@@ -79,7 +79,7 @@ class TestWriter19110Phone < MiniTest::Test
         metadata = hResponseObj[:writerOutput]
         iso_out = Document.new(metadata)
 
-        checkXML = XPath.first(iso_out, '//gmd:phone')
+        checkXML = XPath.first(iso_out, '//gmd:onlineResource')
 
         assert_equal @@aRefXML[2].to_s.squeeze, checkXML.to_s.squeeze
 
