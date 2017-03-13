747452a5e88a42b60d9bafd769bfc4ef5f373b04
stansmith907 <swsak2hi@gmail.com>
2017-03-13 13:59:51 -0800

    Write tests for mdJson writer module 'identifier'

--- test/writers/mdJson/tc_mdjson_identifier.rb
+++ test/writers/mdJson/tc_mdjson_identifier.rb
@@ -1,4 +1,4 @@
-# mdJson 2.0 writer tests - citation
+# mdJson 2.0 writer tests - identifier
 
 # History:
 #   Stan Smith 2017-03-13 refactored for mdJson/mdTranslator 2.0
@@ -9,30 +9,30 @@ require 'json/pure'
 require 'adiwg-mdtranslator'
 require_relative 'mdjson_test_parent'
 
-class TestWriterCitation < TestWriterMdJsonParent
+class TestWriterIdentifier < TestWriterMdJsonParent
 
    # get input JSON for test
-   @@jsonIn = TestWriterMdJsonParent.getJson('citation.json')
+   @@jsonIn = TestWriterMdJsonParent.getJson('identifier.json')
 
-   def test_schema_citation
+   def test_schema_identifier
 
       hIn = JSON.parse(@@jsonIn)
-      hTest = hIn['metadata']['resourceInfo']['citation']
-      errors = TestWriterMdJsonParent.testSchema(hTest, 'citation.json')
+      hTest = hIn['metadata']['resourceInfo']['citation']['identifier'][0]
+      errors = TestWriterMdJsonParent.testSchema(hTest, 'identifier.json')
       assert_empty errors
 
    end
 
-   def test_complete_citation
+   def test_complete_identifier
 
       metadata = ADIWG::Mdtranslator.translate(
          file: @@jsonIn, reader: 'mdJson', validate: 'normal',
          writer: 'mdJson', showAllTags: false)
 
       expect = JSON.parse(@@jsonIn)
-      expect = expect['metadata']['resourceInfo']['citation']
+      expect = expect['metadata']['resourceInfo']['citation']['identifier']
       got = JSON.parse(metadata[:writerOutput])
-      got = got['metadata']['resourceInfo']['citation']
+      got = got['metadata']['resourceInfo']['citation']['identifier']
 
       assert_equal expect, got
 
