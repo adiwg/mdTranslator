e183a2eff0862a1d4ecec8037a08ee7b923d6bea
stansmith907 <swsak2hi@gmail.com>
2017-03-14 23:14:42 -0800

    Refactor mdJson writer module for 'dimension' include minitest modules

---
+++ test/writers/mdJson/tc_mdjson_dimension.rb
@@ -0,0 +1,40 @@
+# mdJson 2.0 writer tests - dimension
+
+# History:
+#   Stan Smith 2017-03-14 original script
+
+require 'minitest/autorun'
+require 'json/pure'
+require 'adiwg-mdtranslator'
+require_relative 'mdjson_test_parent'
+
+class TestWriterDimension < TestWriterMdJsonParent
+
+   # get input JSON for test
+   @@jsonIn = TestWriterMdJsonParent.getJson('dimension.json')
+
+   def test_schema_dimension
+
+      hIn = JSON.parse(@@jsonIn)
+      hTest = hIn['metadata']['resourceInfo']['spatialRepresentation'][0]['gridRepresentation']['dimension'][0]
+      errors = TestWriterMdJsonParent.testSchema(hTest, 'gridRepresentation.json', :fragment=>'dimension')
+      assert_empty errors
+
+   end
+
+   def test_complete_dimension
+
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonIn, reader: 'mdJson', validate: 'normal',
+         writer: 'mdJson', showAllTags: false)
+
+      expect = JSON.parse(@@jsonIn)
+      expect = expect['metadata']['resourceInfo']['spatialRepresentation'][0]['gridRepresentation']['dimension']
+      got = JSON.parse(metadata[:writerOutput])
+      got = got['metadata']['resourceInfo']['spatialRepresentation'][0]['gridRepresentation']['dimension']
+
+      assert_equal expect, got
+
+   end
+
+end
