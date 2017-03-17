279d9ac8decdda3d4ef705a2c9b53f7716830a81
stansmith907 <swsak2hi@gmail.com>
2017-03-16 18:06:27 -0800

    Refactor mdJson writer module for 'imageDescription' include minitest modules

---
+++ test/writers/mdJson/tc_mdjson_image.rb
@@ -0,0 +1,40 @@
+# mdJson 2.0 writer tests - image description
+
+# History:
+#   Stan Smith 2017-03-16 original script
+
+require 'minitest/autorun'
+require 'json/pure'
+require 'adiwg-mdtranslator'
+require_relative 'mdjson_test_parent'
+
+class TestWriterImageDescription < TestWriterMdJsonParent
+
+   # get input JSON for test
+   @@jsonIn = TestWriterMdJsonParent.getJson('image.json')
+
+   def test_schema_image
+
+      hIn = JSON.parse(@@jsonIn)
+      hTest = hIn['metadata']['resourceInfo']['coverageDescription'][0]['imageDescription']
+      errors = TestWriterMdJsonParent.testSchema(hTest, 'imageDescription.json')
+      assert_empty errors
+
+   end
+
+   def test_complete_image
+
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonIn, reader: 'mdJson', validate: 'normal',
+         writer: 'mdJson', showAllTags: false)
+
+      expect = JSON.parse(@@jsonIn)
+      expect = expect['metadata']['resourceInfo']['coverageDescription'][0]
+      got = JSON.parse(metadata[:writerOutput])
+      got = got['metadata']['resourceInfo']['coverageDescription'][0]
+
+      assert_equal expect, got
+
+   end
+
+end
