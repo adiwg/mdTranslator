188db55d3e8d6b50aec0d2b1fcfd0c41db7d3dd9
stansmith907 <swsak2hi@gmail.com>
2017-03-18 11:23:03 -0800

    Refactor mdJson writer module for 'keywordObject' include minitest modules

---
+++ test/writers/mdJson/tc_mdjson_keywordObject.rb
@@ -0,0 +1,25 @@
+# mdJson 2.0 writer tests - keyword object
+
+# History:
+#   Stan Smith 2017-03-18 original script
+
+require 'minitest/autorun'
+require 'json/pure'
+require 'adiwg-mdtranslator'
+require_relative 'mdjson_test_parent'
+
+class TestWriterKeywordObject < TestWriterMdJsonParent
+
+   # get input JSON for test
+   @@jsonIn = TestWriterMdJsonParent.getJson('keyword.json')
+
+   def test_schema_keywordObject
+
+      hIn = JSON.parse(@@jsonIn)
+      hTest = hIn['metadata']['resourceInfo']['keyword'][0]['keyword'][0]
+      errors = TestWriterMdJsonParent.testSchema(hTest, 'keyword.json', :fragment=>'keywordObject')
+      assert_empty errors
+
+   end
+
+end
