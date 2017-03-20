125e660105abf6da438110fb28a77e5c04cc2b26
stansmith907 <swsak2hi@gmail.com>
2017-03-19 16:18:29 -0800

    Refactor mdJson writer module for 'domain', 'entityAttribute' include minitest modules

---
+++ test/writers/mdJson/tc_mdjson_entityAttribute.rb
@@ -0,0 +1,40 @@
+# mdJson 2.0 writer tests - entity attribute
+
+# History:
+#   Stan Smith 2017-03-19 original script
+
+require 'minitest/autorun'
+require 'json/pure'
+require 'adiwg-mdtranslator'
+require_relative 'mdjson_test_parent'
+
+class TestWriterEntityAttribute < TestWriterMdJsonParent
+
+   # get input JSON for test
+   @@jsonIn = TestWriterMdJsonParent.getJson('entityAttribute.json')
+
+   def test_schema_entityAttribute
+
+      hIn = JSON.parse(@@jsonIn)
+      hTest = hIn['dataDictionary'][0]['entity'][0]['attribute'][0]
+      errors = TestWriterMdJsonParent.testSchema(hTest, 'entity.json', :fragment=>'attribute')
+      assert_empty errors
+
+   end
+
+   def test_complete_entityAttribute
+
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonIn, reader: 'mdJson', validate: 'normal',
+         writer: 'mdJson', showAllTags: false)
+
+      expect = JSON.parse(@@jsonIn)
+      expect = expect['dataDictionary'][0]['entity'][0]['attribute']
+      got = JSON.parse(metadata[:writerOutput])
+      got = got['dataDictionary'][0]['entity'][0]['attribute']
+
+      assert_equal expect, got
+
+   end
+
+end
