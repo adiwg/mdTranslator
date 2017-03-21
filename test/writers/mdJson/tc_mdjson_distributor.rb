a46022fa6f22267b395cccfb9bb23c704d7aec2a
stansmith907 <swsak2hi@gmail.com>
2017-03-20 18:19:07 -0800

    Refactor mdJson writer module for 'distributor' include minitest modules

---
+++ test/writers/mdJson/tc_mdjson_distributor.rb
@@ -0,0 +1,40 @@
+# mdJson 2.0 writer tests - distributor
+
+# History:
+#   Stan Smith 2017-03-19 original script
+
+require 'minitest/autorun'
+require 'json/pure'
+require 'adiwg-mdtranslator'
+require_relative 'mdjson_test_parent'
+
+class TestWriterDistributor < TestWriterMdJsonParent
+
+   # get input JSON for test
+   @@jsonIn = TestWriterMdJsonParent.getJson('distributor.json')
+
+   def test_schema_distributor
+
+      hIn = JSON.parse(@@jsonIn)
+      hTest = hIn['metadata']['resourceDistribution'][0]['distributor'][0]
+      errors = TestWriterMdJsonParent.testSchema(hTest, 'distributor.json')
+      assert_empty errors
+
+   end
+
+   def test_complete_distributor
+
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonIn, reader: 'mdJson', validate: 'normal',
+         writer: 'mdJson', showAllTags: false)
+
+      expect = JSON.parse(@@jsonIn)
+      expect = expect['metadata']['resourceDistribution'][0]['distributor']
+      got = JSON.parse(metadata[:writerOutput])
+      got = got['metadata']['resourceDistribution'][0]['distributor']
+
+      assert_equal expect, got
+
+   end
+
+end
