11c25e05dc90e45fec7b7dc60100df78a38a25a7
stansmith907 <adtech907>
2017-04-05 11:19:59 -0800

    Refactor mdTranslator html writer module 'allocation'

--- lib/adiwg/mdtranslator/writers/html/sections/html_allocation.rb
+++ lib/adiwg/mdtranslator/writers/html/sections/html_allocation.rb
@@ -25,10 +25,42 @@ module ADIWG
                   end
 
                   # allocation - currency
+                  unless hAllocation[:currency].nil?
+                     @html.em('Currency: ')
+                     @html.text!(hAllocation[:currency])
+                     @html.br
+                  end
+
                   # allocation - sourceId
+                  unless hAllocation[:sourceId].nil?
+                     hContact = Html_Document.getContact(hAllocation[:sourceId])
+                     @html.em('Source Contact: ')
+                     @html.a(hContact[:contactId], 'href' => '#CID_'+hContact[:contactId])
+                     @html.br
+                  end
+
                   # allocation - recipientId
+                  unless hAllocation[:recipientId].nil?
+                     hContact = Html_Document.getContact(hAllocation[:recipientId])
+                     @html.em('Recipient Contact: ')
+                     @html.a(hContact[:contactId], 'href' => '#CID_'+hContact[:contactId])
+                     @html.br
+                  end
+
                   # allocation - matching {Boolean}
+                  unless hAllocation[:matching].nil?
+                     @html.em('Matching Funds Provided: ')
+                     @html.text!(hAllocation[:matching].to_s)
+                     @html.br
+                  end
+
                   # allocation - comment
+                  unless hAllocation[:comment].nil?
+                     @html.em('Comment: ')
+                     @html.section(:class => 'block') do
+                        @html.text!(hAllocation[:comment])
+                     end
+                  end
 
                end # writeHtml
             end # Html_Allocation
