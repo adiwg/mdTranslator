033f8c016e143f4d529d9f6afb6308068a6cdb56
stansmith907 <swsak2hi@gmail.com>
2017-09-06 17:58:37 -0800

    Add fgdc reader module 'entityOverview' include minitest

---
+++ lib/adiwg/mdtranslator/readers/fgdc/modules/module_entityOverview.rb
@@ -0,0 +1,45 @@
+# Reader - fgdc to internal data structure
+# unpack fgdc entity and attribute overview
+
+# History:
+#  Stan Smith 2017-09-06 original script
+
+require 'uuidtools'
+require 'nokogiri'
+require 'adiwg/mdtranslator/internal/internal_metadata_obj'
+
+module ADIWG
+   module Mdtranslator
+      module Readers
+         module Fgdc
+
+            module EntityOverview
+
+               def self.unpack(xOverview, hResponseObj)
+
+                  # instance classes needed in script
+                  intMetadataClass = InternalMetadata.new
+                  hEntity = intMetadataClass.newEntity
+                  hEntity[:entityId] = UUIDTools::UUID.random_create.to_s
+                  hEntity[:entityCode] = 'overview'
+
+                  # entity attribute 5.2.1 (eaover) - entity attribute overview
+                  # -> dataDictionary.entities.entityDefinition
+                  definition = xOverview.xpath('./eaover').text
+                  unless definition.empty?
+                     hEntity[:entityDefinition] = definition
+                  end
+
+                  # entity attribute 5.2.2 (eadetcit) - entity attribute detail citation []
+                  # -> not mapped
+
+                  return hEntity
+
+               end
+
+            end
+
+         end
+      end
+   end
+end
