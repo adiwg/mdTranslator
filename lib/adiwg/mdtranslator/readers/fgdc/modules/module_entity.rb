463a24736bfd57d434c09ac18002bd66e411e632
stansmith907 <swsak2hi@gmail.com>
2017-09-06 17:57:38 -0800

    Add fgdc reader module 'entity' include minitest

---
+++ lib/adiwg/mdtranslator/readers/fgdc/modules/module_entity.rb
@@ -0,0 +1,72 @@
+# Reader - fgdc to internal data structure
+# unpack fgdc entity detail
+
+# History:
+#  Stan Smith 2017-09-06 original script
+
+require 'uuidtools'
+require 'nokogiri'
+require 'adiwg/mdtranslator/internal/internal_metadata_obj'
+require_relative 'module_attribute'
+
+module ADIWG
+   module Mdtranslator
+      module Readers
+         module Fgdc
+
+            module Entity
+
+               def self.unpack(xDetail, hDictionary, hResponseObj)
+
+                  # instance classes needed in script
+                  intMetadataClass = InternalMetadata.new
+
+                  # entity attribute 5.1.1 (enttype) - definition and description set
+                  xEntity = xDetail.xpath('./enttype')
+                  unless xEntity.empty?
+
+                     hEntity = intMetadataClass.newEntity
+                     hEntity[:entityId] = UUIDTools::UUID.random_create.to_s
+
+                     # entity attribute 5.1.1.1 (enttypl) - entity name
+                     # -> dataDictionary.entities.entityCode
+                     code = xEntity.xpath('./enttypl').text
+                     unless code.empty?
+                        hEntity[:entityCode] = code
+                     end
+
+                     # entity attribute 5.1.1.2 (enttypd) - entity definition
+                     # -> dataDictionary.entities.entityDefinition
+                     definition = xEntity.xpath('./enttypd').text
+                     unless definition.empty?
+                        hEntity[:entityDefinition] = definition
+                     end
+
+                     # entity attribute 5.1.1.3 (enttypds) - entity definition source
+                     # -> not mapped
+
+                     # entity attribute 5.1.2 (attr) - characteristics of an attribute
+                     axAttribute = xDetail.xpath('./attr')
+                     unless axAttribute.empty?
+                        axAttribute.each do |xAttribute|
+                           hAttribute = Attribute.unpack(xAttribute, hDictionary, hResponseObj)
+                           unless hAttribute.nil?
+                              hEntity[:attributes] << hAttribute
+                           end
+                        end
+                     end
+
+                     return hEntity
+
+                  end
+
+                  return nil
+
+               end
+
+            end
+
+         end
+      end
+   end
+end
