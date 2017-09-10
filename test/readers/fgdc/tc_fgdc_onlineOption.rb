89a3e36e3c2f064db72cbcf6ed7b2ff5c86369fb
stansmith907 <swsak2hi@gmail.com>
2017-09-10 14:24:30 -0800

    Add fgdc reader module 'online' include minitest

---
+++ test/readers/fgdc/tc_fgdc_onlineOption.rb
@@ -0,0 +1,53 @@
+# MdTranslator - minitest of
+# readers / fgdc / module_onlineOption
+
+# History:
+#   Stan Smith 2017-09-10 original script
+
+require 'nokogiri'
+require 'adiwg/mdtranslator/internal/internal_metadata_obj'
+require 'adiwg/mdtranslator/readers/fgdc/modules/module_fgdc'
+require_relative 'fgdc_test_parent'
+
+class TestReaderFgdcOnlineOption < TestReaderFGDCParent
+
+   @@xDoc = TestReaderFGDCParent.get_XML('distribution.xml')
+   @@NameSpace = ADIWG::Mdtranslator::Readers::Fgdc::Distribution
+
+   def test_onlineOption_complete
+
+      intMetadataClass = InternalMetadata.new
+      hResourceInfo = intMetadataClass.newResourceInfo
+
+      TestReaderFGDCParent.set_xDoc(@@xDoc)
+      TestReaderFGDCParent.set_intObj
+      xIn = @@xDoc.xpath('./metadata/distinfo[1]')
+      hDistribution = @@NameSpace.unpack(xIn, hResourceInfo, @@hResponseObj)
+
+      refute_empty hDistribution
+
+      hDistributor = hDistribution[:distributor][0]
+      refute_empty hDistributor
+
+      aTransfer = hDistributor[:transferOptions]
+      assert_equal 3, aTransfer.length
+
+      hTransfer0 = aTransfer[0]
+      assert_equal 5, hTransfer0[:onlineOptions].length
+
+      hOnline0 = hTransfer0[:onlineOptions][0]
+      assert_equal 'https://doi.org/10.5066/1', hOnline0[:olResURI]
+      assert_equal 'online access instructions', hOnline0[:olResProtocol]
+      assert_nil hOnline0[:olResName]
+      assert_equal 'online computer and OS', hOnline0[:olResDesc]
+      assert_nil hOnline0[:olResFunction]
+
+      assert_equal 'https://doi.org/10.5066/3', hTransfer0[:onlineOptions][2][:olResURI]
+      assert_equal 'https://doi.org/10.5066/4', hTransfer0[:onlineOptions][3][:olResURI]
+
+      assert @@hResponseObj[:readerExecutionPass]
+      assert_empty @@hResponseObj[:readerExecutionMessages]
+
+   end
+
+end
