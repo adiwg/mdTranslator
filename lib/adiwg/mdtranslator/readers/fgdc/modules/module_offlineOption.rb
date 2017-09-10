cff9137e203291a02cc7bc04604eb1d94a83bc27
stansmith907 <swsak2hi@gmail.com>
2017-09-10 14:24:10 -0800

    Add fgdc reader module 'offlineOption' include minitest

---
+++ lib/adiwg/mdtranslator/readers/fgdc/modules/module_offlineOption.rb
@@ -0,0 +1,98 @@
+# Reader - fgdc to internal data structure
+# unpack fgdc distribution digital offline transfer
+
+# History:
+#  Stan Smith 2017-09-09 original script
+
+require 'nokogiri'
+require 'adiwg/mdtranslator/internal/internal_metadata_obj'
+require_relative 'module_date'
+
+module ADIWG
+   module Mdtranslator
+      module Readers
+         module Fgdc
+
+            module OfflineOption
+
+               def self.unpack(xOffline, hResponseObj)
+
+                  # instance classes needed in script
+                  intMetadataClass = InternalMetadata.new
+                  hOffline = intMetadataClass.newMedium
+                  aDensity = []
+                  aOfflines = []
+
+                  # distribution 6.4.2.2.2.1 (offmedia) - offline media
+                  # -> distribution.distributor.offlineOption.mediumSpecification.title
+                  title = xOffline.xpath('./offmedia').text
+                  unless title.empty?
+                     hSpecification = intMetadataClass.newCitation
+                     hSpecification[:title] = title
+                     hOffline[:mediumSpecification] = hSpecification
+                  end
+
+                  # distribution 6.4.2.2.2.2 (reccap) - recording capacity
+                  xCapacity = xOffline.xpath('./reccap')
+                  unless xCapacity.empty?
+
+                     # distribution 6.4.2.2.2.2.1 (recden) - recording density []
+                     # -> distribution.distributor.offlineOption.density
+                     axDensity = xCapacity.xpath('./recden')
+                     unless axDensity.empty?
+                        axDensity.each do |xDensity|
+                           density = xDensity.text
+                           unless density.empty?
+                              aDensity << density.to_i
+                           end
+                        end
+                     end
+
+                     # distribution 6.4.2.2.2.2.2 (recdenu) - recording density units
+                     # -> distribution.distributor.offlineOption.units
+                     units = xCapacity.xpath('./recdenu').text
+                     unless units.empty?
+                        hOffline[:units] = units
+                     end
+
+                  end
+
+                  # distribution 6.4.2.2.2.3 (recfmt) - recording format []
+                  # -> distribution.distributor.offlineOption.mediumFormat
+                  axFormat = xOffline.xpath('./recfmt')
+                  unless axFormat.empty?
+                     axFormat.each do |xFormat|
+                        format = xFormat.text
+                        unless format.empty?
+                           hOffline[:mediumFormat] << format
+                        end
+                     end
+                  end
+
+                  # distribution 6.4.2.2.2.4 (compat) - compatibility information
+                  # -> distribution.distributor.offlineOption.note
+                  note = xOffline.xpath('./compat').text
+                  unless note.empty?
+                     hOffline[:note] = note
+                  end
+
+                  # create an offline object for each density
+                  if aDensity.empty?
+                     aOfflines << hOffline
+                  else
+                     aDensity.each do |density|
+                        hClone = hOffline.clone
+                        hClone[:density] = density
+                        aOfflines << hClone
+                     end
+                  end
+
+                  return aOfflines
+
+               end
+            end
+
+         end
+      end
+   end
+end
---
+++ test/readers/fgdc/tc_fgdc_offlineOption.rb
@@ -0,0 +1,57 @@
+# MdTranslator - minitest of
+# readers / fgdc / module_offlineOption
+
+# History:
+#   Stan Smith 2017-09-10 original script
+
+require 'nokogiri'
+require 'adiwg/mdtranslator/internal/internal_metadata_obj'
+require 'adiwg/mdtranslator/readers/fgdc/modules/module_fgdc'
+require_relative 'fgdc_test_parent'
+
+class TestReaderFgdcOfflineOption < TestReaderFGDCParent
+
+   @@xDoc = TestReaderFGDCParent.get_XML('distribution.xml')
+   @@NameSpace = ADIWG::Mdtranslator::Readers::Fgdc::Distribution
+
+   def test_offlineOption_complete
+
+      intMetadataClass = InternalMetadata.new
+      hResourceInfo = intMetadataClass.newResourceInfo
+
+      TestReaderFGDCParent.set_xDoc(@@xDoc)
+      TestReaderFGDCParent.set_intObj
+      xIn = @@xDoc.xpath('./metadata/distinfo[1]')
+      hDistribution = @@NameSpace.unpack(xIn, hResourceInfo, @@hResponseObj)
+
+      refute_empty hDistribution
+
+      hDistributor = hDistribution[:distributor][0]
+      refute_empty hDistributor
+
+      aTransfer = hDistributor[:transferOptions]
+      assert_equal 3, aTransfer.length
+
+      hTransfer0 = aTransfer[0]
+      assert_equal 4, hTransfer0[:offlineOptions].length
+
+      hOffline0 = hTransfer0[:offlineOptions][0]
+      refute_empty hOffline0[:mediumSpecification]
+      assert_equal 25, hOffline0[:density]
+      assert_equal 'GB', hOffline0[:units]
+      assert_nil hOffline0[:numberOfVolumes]
+      assert_equal 2, hOffline0[:mediumFormat].length
+      assert_equal 'medium format 1', hOffline0[:mediumFormat][0]
+      assert_equal 'medium format 2', hOffline0[:mediumFormat][1]
+      assert_equal 'medium compatibility information', hOffline0[:note]
+      assert_empty hOffline0[:identifier]
+
+      hSpecification = hOffline0[:mediumSpecification]
+      assert_equal 'medium 1', hSpecification[:title]
+
+      assert @@hResponseObj[:readerExecutionPass]
+      assert_empty @@hResponseObj[:readerExecutionMessages]
+
+   end
+
+end
