6482cf43b9744b75c805b1b2e421f3391c840bb5
stansmith907 <swsak2hi@gmail.com>
2017-10-17 12:26:21 -0800

    Fix issue when concatenating technical prerequisites to nil distribution description.

--- lib/adiwg/mdtranslator/version.rb
+++ lib/adiwg/mdtranslator/version.rb
@@ -1,6 +1,7 @@
 # adiwg mdTranslator
 
 # version 2 history
+# 2.3.5 2017-10-17 fixed problem with adding technical prerequisite to nil distribution description
 # 2.3.5 2017-10-13 trap missing topology level in html writer vectorRepresentation
 # 2.3.4 2017-10-12 drop harvest set tag if repository citation is missing
 # 2.3.3 2017-10-03 modify sbJson reader execution fail tests
