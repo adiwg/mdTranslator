2b97fc54f3059a87390ebc36a7c6774f5fee5a43
stansmith907 <swsak2hi@gmail.com>
2017-10-20 14:00:20 -0800

    Add html writer for 'bearingDistanceResolution'

---
+++ lib/adiwg/mdtranslator/writers/html/sections/html_coordinateResolution.rb
@@ -0,0 +1,47 @@
+# HTML writer
+# coordinate resolution
+
+# History:
+#  Stan Smith 2017-10-20 original script
+
+module ADIWG
+   module Mdtranslator
+      module Writers
+         module Html
+
+            class Html_CoordinateResolution
+
+               def initialize(html)
+                  @html = html
+               end
+
+               def writeHtml(hCoordRes)
+
+                  # coordinate resolution - abscissa
+                  unless hCoordRes[:abscissaResolutionX].nil?
+                     @html.em('X Resolution (abscissa): ')
+                     @html.text!(hCoordRes[:abscissaResolutionX].to_s)
+                     @html.br
+                  end
+
+                  # coordinate resolution - ordinate
+                  unless hCoordRes[:ordinateResolutionY].nil?
+                     @html.em('Y Resolution (ordinate): ')
+                     @html.text!(hCoordRes[:ordinateResolutionY].to_s)
+                     @html.br
+                  end
+
+                  # coordinate resolution - unit of measure
+                  unless hCoordRes[:unitOfMeasure].nil?
+                     @html.em('Units of Measure: ')
+                     @html.text!(hCoordRes[:unitOfMeasure])
+                     @html.br
+                  end
+
+               end # writeHtml
+            end # Html_CoordinateResolution
+
+         end
+      end
+   end
+end
