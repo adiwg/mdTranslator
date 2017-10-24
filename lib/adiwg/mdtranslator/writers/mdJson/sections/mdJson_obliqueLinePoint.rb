62e3f866be0a09b933f6d2c2fc67606aa8be1645
stansmith907 <swsak2hi@gmail.com>
2017-10-24 13:37:41 -0800

    Add mdJson writer for 'obliqueLinePoint' include minitest

---
+++ lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_obliqueLinePoint.rb
@@ -0,0 +1,28 @@
+# mdJson 2.0 writer - spatial reference system oblique line point
+
+# History:
+#   Stan Smith 2017-10-24 original script
+
+require 'jbuilder'
+
+module ADIWG
+   module Mdtranslator
+      module Writers
+         module MdJson
+
+            module ObliqueLinePoint
+
+               def self.build(hLinePoint)
+
+                  Jbuilder.new do |json|
+                     json.azimuthLineLatitude hLinePoint[:azimuthLineLatitude]
+                     json.azimuthLineLongitude hLinePoint[:azimuthLineLongitude]
+                  end
+
+               end # build
+            end # ObliqueLinePoint
+
+         end
+      end
+   end
+end
