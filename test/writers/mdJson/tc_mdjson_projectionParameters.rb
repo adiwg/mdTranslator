5cd8b7c8fe35d5aa3c5c8fe8d6ff3312b8fb0670
stansmith907 <swsak2hi@gmail.com>
2017-10-24 13:36:14 -0800

    Add mdJson writer for 'projectionParameters' include minitest

---
+++ test/writers/mdJson/tc_mdjson_projectionParameters.rb
@@ -0,0 +1,42 @@
+# mdJson 2.0 writer tests - spatial reference projection parameters
+
+# History:
+#   Stan Smith 2017-10-24 original script
+
+require 'minitest/autorun'
+require 'json'
+require 'adiwg-mdtranslator'
+require_relative 'mdjson_test_parent'
+
+class TestWriterMdJsonProjectionParameters < TestWriterMdJsonParent
+
+   # get input JSON for test
+   @@jsonIn = TestWriterMdJsonParent.getJson('projectionParameters.json')
+
+   # TODO complete after schema update
+   # def test_schema_spatialReferenceParameters
+   #
+   #    hIn = JSON.parse(@@jsonIn)
+   #    hTest = hIn['metadata']['resourceInfo']['spatialReferenceSystem'][0]
+   #    errors = TestWriterMdJsonParent.testSchema(hTest, 'spatialReference.json')
+   #    assert_empty errors
+   #
+   # end
+
+   def test_complete_projectionParameters
+
+      # TODO validate normal after schema update
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonIn, reader: 'mdJson', validate: 'none',
+         writer: 'mdJson', showAllTags: false)
+
+      expect = JSON.parse(@@jsonIn)
+      expect = expect['metadata']['resourceInfo']['spatialReferenceSystem'][0]['referenceSystemParameterSet']['projection']
+      got = JSON.parse(metadata[:writerOutput])
+      got = got['metadata']['resourceInfo']['spatialReferenceSystem'][0]['referenceSystemParameterSet']['projection']
+
+      assert_equal expect, got
+
+   end
+
+end
