76b08ea2d4c9203a54b616e48deffe09a0e8cac3
stansmith907 <swsak2hi@gmail.com>
2017-10-25 15:29:43 -0800

    Add html writer for new spatial reference system parameters include minitest

---
+++ lib/adiwg/mdtranslator/writers/html/sections/html_verticalDatumParameters.rb
@@ -0,0 +1,69 @@
+# HTML writer
+# spatial reference system vertical datum parameters
+
+# History:
+#  Stan Smith 2017-10-24 original script
+
+require_relative 'html_identifier'
+
+module ADIWG
+   module Mdtranslator
+      module Writers
+         module Html
+
+            class Html_VerticalDatumParameters
+
+               def initialize(html)
+                  @html = html
+               end
+
+               def writeHtml(hDatum)
+
+                  # classes used
+                  identifierClass = Html_Identifier.new(@html)
+
+                  # vertical datum - is depth system {Boolean}
+                  if hDatum[:encodingMethod]
+                     @html.text!('Depth System')
+                  else
+                     @html.text!('Altitude System')
+                  end
+                  @html.br
+
+                  # vertical datum - encoding method
+                  unless hDatum[:encodingMethod].nil?
+                     @html.em('Encoding Method: ')
+                     @html.text!(hDatum[:encodingMethod])
+                     @html.br
+                  end
+
+                  # vertical datum - vertical resolution
+                  unless hDatum[:verticalResolution].nil?
+                     @html.em('Vertical Resolution: ')
+                     @html.text!(hDatum[:verticalResolution].to_s)
+                     @html.br
+                  end
+
+                  # vertical datum - unit of measure
+                  unless hDatum[:unitOfMeasure].nil?
+                     @html.em('Unit of Measure: ')
+                     @html.text!(hDatum[:unitOfMeasure])
+                  end
+
+                  # vertical datum parameters - vertical datum identifier
+                  unless hDatum[:datumIdentifier].empty?
+                     @html.details do
+                        @html.summary('Vertical Datum Identifier', {'id' => 'datum-identifier', 'class' => 'h5'})
+                        @html.section(:class => 'block') do
+                           identifierClass.writeHtml(hDatum[:datumIdentifier])
+                        end
+                     end
+                  end
+
+               end # writeHtml
+            end # Html_VerticalDatumParameters
+
+         end
+      end
+   end
+end
--- lib/adiwg/mdtranslator/writers/iso19115_2/classes/class_referenceSystem.rb
+++ lib/adiwg/mdtranslator/writers/iso19115_2/classes/class_referenceSystem.rb
@@ -3,49 +3,49 @@
 # 19115-2 output for ISO 19115-2 XML
 
 # History:
-#   Stan Smith 2016-12-07 refactored for mdTranslator/mdJson 2.0
-#   Stan Smith 2015-08-28 convert referenceSystem to resourceId and pass to RS_Identifier
-#   Stan Smith 2015-07-14 refactored to eliminate namespace globals $WriterNS and $IsoNS
-#   Stan Smith 2015-07-14 refactored to make iso19110 independent of iso19115_2 classes
-#   Stan Smith 2015-06-22 replace global ($response) with passed in object (hResponseObj)
-#   Stan Smith 2014-12-12 refactored to handle namespacing readers and writers
+#  Stan Smith 2016-12-07 refactored for mdTranslator/mdJson 2.0
+#  Stan Smith 2015-08-28 convert referenceSystem to resourceId and pass to RS_Identifier
+#  Stan Smith 2015-07-14 refactored to eliminate namespace globals $WriterNS and $IsoNS
+#  Stan Smith 2015-07-14 refactored to make iso19110 independent of iso19115_2 classes
+#  Stan Smith 2015-06-22 replace global ($response) with passed in object (hResponseObj)
+#  Stan Smith 2014-12-12 refactored to handle namespacing readers and writers
 # 	Stan Smith 2014-09-03 original script
 
 require_relative 'class_rsIdentifier'
 
 module ADIWG
-    module Mdtranslator
-        module Writers
-            module Iso19115_2
+   module Mdtranslator
+      module Writers
+         module Iso19115_2
 
-                class MD_ReferenceSystem
+            class MD_ReferenceSystem
 
-                    def initialize(xml, hResponseObj)
-                        @xml = xml
-                        @hResponseObj = hResponseObj
-                    end
+               def initialize(xml, hResponseObj)
+                  @xml = xml
+                  @hResponseObj = hResponseObj
+               end
 
-                    def writeXML(hSystem)
+               def writeXML(hSystem)
 
-                        # classes used
-                        idClass =  RS_Identifier.new(@xml, @hResponseObj)
+                  # classes used
+                  idClass = RS_Identifier.new(@xml, @hResponseObj)
 
-                        hIdentifier = hSystem[:systemIdentifier]
-                        unless hIdentifier.empty?
-                            @xml.tag!('gmd:MD_ReferenceSystem') do
-                                @xml.tag!('gmd:referenceSystemIdentifier') do
-                                    idClass.writeXML(hIdentifier)
-                                end
-                            end
+                  hIdentifier = hSystem[:systemIdentifier]
+                  unless hIdentifier.empty?
+                     @xml.tag!('gmd:MD_ReferenceSystem') do
+                        @xml.tag!('gmd:referenceSystemIdentifier') do
+                           idClass.writeXML(hIdentifier)
                         end
-                        if hIdentifier.empty? && @hResponseObj[:writerShowTags]
-                            @xml.tag!('gmd:MD_ReferenceSystem')
-                        end
+                     end
+                  end
+                  if hIdentifier.empty? && @hResponseObj[:writerShowTags]
+                     @xml.tag!('gmd:MD_ReferenceSystem')
+                  end
 
-                    end # writeXML
-                end # MD_ReferenceSystem class
+               end # writeXML
+            end # MD_ReferenceSystem class
 
-            end
-        end
-    end
+         end
+      end
+   end
 end
