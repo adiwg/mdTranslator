c6da18ca223fec13c869b5b11d01688b62bd8dcf
stansmith907 <swsak2hi@gmail.com>
2017-11-22 16:03:23 -0900

    Add 'citation' to fgdc writer include minitest

--- lib/adiwg/mdtranslator/writers/fgdc/classes/class_citation.rb
+++ lib/adiwg/mdtranslator/writers/fgdc/classes/class_citation.rb
@@ -4,6 +4,11 @@
 # History:
 #   Stan Smith 2017-11-17 original script
 
+require 'adiwg/mdtranslator/internal/module_dateTimeFun'
+require_relative 'class_series'
+require_relative 'class_publisher'
+require_relative 'class_citation'
+
 module ADIWG
    module Mdtranslator
       module Writers
@@ -16,21 +21,33 @@ module ADIWG
                   @hResponseObj = hResponseObj
                end
 
-               def writeXML(hCitation)
+               def find_responsibility(hCitation, roleName)
+                  aParties = []
+                  hCitation[:responsibleParties].each do |hRParty|
+                     if hRParty[:roleName] == roleName
+                        hRParty[:parties].each do |hParty|
+                           aParties << hParty[:contactId]
+                        end
+                     end
+                  end
+                  aParties = aParties.uniq
+                  return aParties
+               end
+
+               def writeXML(hCitation, aAssocResource)
+
+                  seriesClass = Series.new(@xml, @hResponseObj)
+                  pubClass = Publisher.new(@xml, @hResponseObj)
+                  citationClass = Citation.new(@xml, @hResponseObj)
 
                   @xml.tag!('citeinfo') do
 
-                     # citation 1.1.1 (origin) - originator [] (required)
+                     # citation 8.1 (origin) - originator [] (required)
                      # <- hCitation[:responsibleParties] role = 'originator'
                      haveOriginator = false
-                     aOriginators = []
-                     hCitation[:responsibleParties].each do |hRParty|
-                        if hRParty[:roleName] == 'originator'
-                           aOriginators = aOriginators.concat(hRParty[:parties])
-                        end
-                     end
-                     aOriginators.each do |hParty|
-                        hContact = ADIWG::Mdtranslator::Writers::Fgdc.getContact(hParty[:contactId])
+                     aOriginators = find_responsibility(hCitation, 'originator')
+                     aOriginators.each do |contactId|
+                        hContact = ADIWG::Mdtranslator::Writers::Fgdc.getContact(contactId)
                         unless hContact.empty?
                            name = hContact[:name]
                            unless name.nil?
@@ -44,16 +61,106 @@ module ADIWG
                         @hResponseObj[:writerMessages] << 'Citation is missing originator'
                      end
 
-                     # citation 1.1.2 (pubdate) - publication date (required)
-                     # citation 1.1.3 (pubtime) - publication time
-                     # citation 1.1.4 (title) - title (required)
-                     # citation 1.1.5 (edition) - edition
-                     # citation 1.1.6 (geoform) - geospatial data presentation form
-                     # citation 1.1.7 (serinfo) - series information
-                     # citation 1.1.8 (pubinfo) - publication information
-                     # citation 1.1.9 (othercit) - other citation details
-                     # citation 1.1.10 (onlink) - online linkage []
-                     # citation 1.1.11 (lworkcit) - larger work citation
+                     # citation 8.2 (pubdate) - publication date (required)
+                     # citation 8.3 (pubtime) - publication time
+                     # <- hCitation[:dates] dateType = 'publication'
+                     havePubDate = false
+                     hCitation[:dates].each do |hDate|
+                        unless hDate.empty?
+                           unless hDate[:dateType].nil?
+                              if hDate[:dateType] == 'publication'
+                                 pubDate = AdiwgDateTimeFun.stringDateFromDateTime(hDate[:date], hDate[:dateResolution])
+                                 pubTime = AdiwgDateTimeFun.stringTimeFromDateTime(hDate[:date], hDate[:dateResolution])
+                                 unless pubDate == 'ERROR'
+                                    @xml.tag!('pubdate', pubDate)
+                                    havePubDate = true
+                                 end
+                                 unless pubTime == 'ERROR'
+                                    @xml.tag!('pubtime', pubTime)
+                                 end
+                                 break
+                              end
+                           end
+                        end
+                     end
+                     unless havePubDate
+                        @hResponseObj[:writerPass] = false
+                        @hResponseObj[:writerMessages] << 'Citation is missing publication date'
+                     end
+
+                     # citation 8.4 (title) - title (required)
+                     # <- hCitation[:title]
+                     unless hCitation[:title].nil? || hCitation[:title] == ''
+                        @xml.tag!('title', hCitation[:title])
+                     end
+                     if hCitation[:title].nil? || hCitation[:title] == ''
+                        @hResponseObj[:writerPass] = false
+                        @hResponseObj[:writerMessages] << 'Citation is missing title'
+                     end
+
+                     # citation 8.5 (edition) - edition
+                     # <- hCitation[:edition]
+                     unless hCitation[:edition].nil?
+                        @xml.tag!('edition', hCitation[:edition])
+                     end
+
+                     # citation 8.6 (geoform) - geospatial data presentation form
+                     # <- hCitation[:presentationForm] [] - take first
+                     hCitation[:presentationForms].each do |geoForm|
+                        unless geoForm.empty?
+                           @xml.tag!('geoform', geoForm)
+                           break
+                        end
+                     end
+
+                     # citation 8.7 (serinfo) - series information
+                     # <- hCitation[:series]
+                     unless hCitation[:series].empty?
+                        @xml.tag!('serinfo') do
+                           seriesClass.writeXML(hCitation[:series])
+                        end
+                     end
+
+                     # citation 8.8 (pubinfo) - publication information
+                     # <- hCitation[:responsibleParties] role = 'publisher'
+                     # only take first publisher
+                     aPublisher = find_responsibility(hCitation, 'publisher')
+                     unless aPublisher.empty?
+                        hContact = ADIWG::Mdtranslator::Writers::Fgdc.getContact(aPublisher[0])
+                        unless hContact.empty?
+                           @xml.tag!('pubinfo') do
+                              pubClass.writeXML(hContact)
+                           end
+                        end
+                     end
+
+                     # citation 8.9 (othercit) - other citation details
+                     # <- hCitation[:otherDetails][0] - take first
+                     unless hCitation[:otherDetails].empty?
+                        @xml.tag!('othercit', hCitation[:otherDetails][0])
+                     end
+
+                     # citation 8.10 (onlink) - online linkage []
+                     # <- hCitation[:onlineResources][:uri]
+                     hCitation[:onlineResources].each do |hOnline|
+                        unless hOnline[:olResURI].nil?
+                           @xml.tag!('onlink', hOnline[:olResURI])
+                        end
+                     end
+
+                     # citation 8.11 (lworkcit) - larger work citation
+                     # <- associatedResource[] - associationType = 'largerWorkCitation' take first
+                     # <- associatedResource[:resourceCitation]
+                     aAssocResource.each do |hResource|
+                        if hResource[:associationType] == 'largerWorkCitation'
+                           unless hResource[:resourceCitation].empty?
+                              @xml.tag!('lworkcit') do
+                                 citationClass.writeXML(hResource[:resourceCitation], [])
+                              end
+                              break
+                           end
+                        end
+                     end
 
                   end # citeinfo tag
                end # writeXML
--- test/writers/fgdc/tc_citation.rb
+++ test/writers/fgdc/tc_citation.rb
@@ -4,31 +4,128 @@
 # History:
 #   Stan Smith 2017-11-17 original script
 
-require 'minitest/autorun'
-require 'json'
-require 'rubygems'
-require 'adiwg/mdtranslator'
 require_relative 'fgdc_test_parent'
 
 class TestWriterFgdcCitation < TestReaderFgdcParent
 
    # read the mdJson 2.0 file
-   @@mdJson = TestReaderFgdcParent.get_file('citation.json')
+   @@mdJson = TestReaderFgdcParent.get_json('citation')
 
-   def test_fgdc
+   def test_citation_complete
 
-      # read the fgdc reference file
-      xFile = TestReaderFgdcParent.get_xml('citation.xml')
-      xExpect = xFile.xpath('./metadata/idinfo/citation')
+      aReturn = TestReaderFgdcParent.get_complete('citation', './metadata/idinfo/citation')
+      assert_equal aReturn[0], aReturn[1]
+
+   end
+
+   def test_citation_originator_empty
+
+      hIn = Marshal::load(Marshal.dump(@@mdJson))
+      hIn['metadata']['resourceInfo']['citation']['responsibleParty'] = []
+      hIn = hIn.to_json
 
       hResponseObj = ADIWG::Mdtranslator.translate(
-         file: @@mdJson, reader: 'mdJson', writer: 'fgdc', showAllTags: true
+         file: hIn, reader: 'mdJson', writer: 'fgdc', showAllTags: true
+      )
+
+      refute hResponseObj[:writerPass]
+      refute_empty hResponseObj[:writerMessages]
+
+   end
+
+   def test_citation_originator_missing
+
+      hIn = Marshal::load(Marshal.dump(@@mdJson))
+      hIn['metadata']['resourceInfo']['citation'].delete('responsibleParty')
+      hIn = hIn.to_json
+
+      hResponseObj = ADIWG::Mdtranslator.translate(
+         file: hIn, reader: 'mdJson', writer: 'fgdc', showAllTags: true
+      )
+
+      refute hResponseObj[:writerPass]
+      refute_empty hResponseObj[:writerMessages]
+
+   end
+
+   def test_citation_publication_date_format
+
+      # year only
+      hIn = Marshal::load(Marshal.dump(@@mdJson))
+      hIn['metadata']['resourceInfo']['citation']['date'][1]['date'] = '2014'
+      hIn = hIn.to_json
+
+      hResponseObj = ADIWG::Mdtranslator.translate(
+         file: hIn, reader: 'mdJson', writer: 'fgdc', showAllTags: true
       )
 
       xMetadata = Nokogiri::XML(hResponseObj[:writerOutput])
-      xGot = xMetadata.xpath('./metadata/idinfo/citation')
+      xGot = xMetadata.xpath('./metadata/idinfo/citation/citeinfo/pubdate').text
+      xNot = xMetadata.xpath('./metadata/idinfo/citation/citeinfo/pubtime').text
 
-      assert_equal xExpect.to_s.squeeze, xGot.to_s.squeeze
+      assert_equal '2014', xGot
+      assert_empty xNot
+
+      # year-month only
+      hIn = Marshal::load(Marshal.dump(@@mdJson))
+      hIn['metadata']['resourceInfo']['citation']['date'][1]['date'] = '2015-11'
+      hIn = hIn.to_json
+
+      hResponseObj = ADIWG::Mdtranslator.translate(
+         file: hIn, reader: 'mdJson', writer: 'fgdc', showAllTags: true
+      )
+
+      xMetadata = Nokogiri::XML(hResponseObj[:writerOutput])
+      xGot = xMetadata.xpath('./metadata/idinfo/citation/citeinfo/pubdate').text
+      xNot = xMetadata.xpath('./metadata/idinfo/citation/citeinfo/pubtime').text
+
+      assert_equal '2015-11', xGot
+      assert_empty xNot
+
+      # year-month-day only
+      hIn = Marshal::load(Marshal.dump(@@mdJson))
+      hIn['metadata']['resourceInfo']['citation']['date'][1]['date'] = '2016-12-25'
+      hIn = hIn.to_json
+
+      hResponseObj = ADIWG::Mdtranslator.translate(
+         file: hIn, reader: 'mdJson', writer: 'fgdc', showAllTags: true
+      )
+
+      xMetadata = Nokogiri::XML(hResponseObj[:writerOutput])
+      xGot = xMetadata.xpath('./metadata/idinfo/citation/citeinfo/pubdate').text
+
+      assert_equal '2016-12-25', xGot
+      assert_empty xNot
+
+   end
+
+   def test_citation_publication_date_empty
+
+      hIn = Marshal::load(Marshal.dump(@@mdJson))
+      hIn['metadata']['resourceInfo']['citation']['date'] = []
+      hIn = hIn.to_json
+
+      hResponseObj = ADIWG::Mdtranslator.translate(
+         file: hIn, reader: 'mdJson', writer: 'fgdc', showAllTags: true
+      )
+
+      refute hResponseObj[:writerPass]
+      refute_empty hResponseObj[:writerMessages]
+
+   end
+
+   def test_citation_publication_date_missing
+
+      hIn = Marshal::load(Marshal.dump(@@mdJson))
+      hIn['metadata']['resourceInfo']['citation'].delete('date')
+      hIn = hIn.to_json
+
+      hResponseObj = ADIWG::Mdtranslator.translate(
+         file: hIn, reader: 'mdJson', writer: 'fgdc', showAllTags: true
+      )
+
+      refute hResponseObj[:writerPass]
+      refute_empty hResponseObj[:writerMessages]
 
    end
 
