207b62317ca4e674bf8ca9eddb7f8cbf8a4fae30
stansmith907 <swsmith@gci.net>
2019-04-18 13:05:02 -0800

    ISO 19115-1 minitests for constraint

--- lib/adiwg/mdtranslator/writers/iso19115_1/classes/class_constraint.rb
+++ lib/adiwg/mdtranslator/writers/iso19115_1/classes/class_constraint.rb
@@ -4,6 +4,7 @@
 # History:
 # 	Stan Smith 2019-03-15 original script
 
+require_relative '../iso19115_1_writer'
 require_relative 'class_useConstraints'
 require_relative 'class_legalConstraints'
 require_relative 'class_securityConstraints'
@@ -18,18 +19,28 @@ module ADIWG
                def initialize(xml, hResponseObj)
                   @xml = xml
                   @hResponseObj = hResponseObj
+                  @NameSpace = ADIWG::Mdtranslator::Writers::Iso19115_1
                end
 
                def writeXML(hConstraint, inContext = nil)
+
+                  outContext = 'constraint'
+                  outContext = inContext + ' constraint' unless inContext.nil?
 
                   # classes used
                   useConClass = MD_Constraints.new(@xml, @hResponseObj)
                   legalConClass = MD_LegalConstraints.new(@xml, @hResponseObj)
                   securityConClass = MD_SecurityConstraints.new(@xml, @hResponseObj)
 
-                  useConClass.writeXML(hConstraint, inContext) if hConstraint[:type] == 'use'
-                  legalConClass.writeXML(hConstraint, inContext) if hConstraint[:type] == 'legal'
-                  securityConClass.writeXML(hConstraint, inContext) if hConstraint[:type] == 'security'
+                  if hConstraint[:type] == 'use'
+                     useConClass.writeXML(hConstraint, outContext)
+                  elsif hConstraint[:type] == 'legal'
+                     legalConClass.writeXML(hConstraint, outContext)
+                  elsif hConstraint[:type] == 'security'
+                     securityConClass.writeXML(hConstraint, outContext)
+                  else
+                     @NameSpace.issueWarning(293, nil, outContext + " type = #{hConstraint[:type]}")
+                  end
 
                end # writeXML
             end # MD_Constraint class
