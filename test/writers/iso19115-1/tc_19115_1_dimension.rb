b2517b3134cb7e217a0b449af22ec85c13681b93
stansmith907 <swsmith@gci.net>
2019-04-24 11:17:44 -0800

    ISO 19115-1 minitests for dimension

--- test/writers/iso19115-1/tc_19115_1_dimension.rb
+++ test/writers/iso19115-1/tc_19115_1_dimension.rb
@@ -1,16 +1,14 @@
 # MdTranslator - minitest of
-# writers / iso19115_2 / class_dimension
+# writers / iso19115_1 / class_dimension
 
 # History:
-#  Stan Smith 2018-04-19 refactored for error messaging
-#  Stan Smith 2017-11-19 replace REXML with Nokogiri
-#  Stan Smith 2016-11-23 original script
+#  Stan Smith 2019-04-24 original script
 
 require_relative '../../helpers/mdJson_hash_objects'
 require_relative '../../helpers/mdJson_hash_functions'
-require_relative 'iso19115_2_test_parent'
+require_relative 'iso19115_1_test_parent'
 
-class TestWriter191152Dimension < TestWriter191152Parent
+class TestWriter191151Dimension < TestWriter191151Parent
 
    # instance classes needed in script
    TDClass = MdJsonHashWriter.new
@@ -29,9 +27,9 @@ class TestWriter191152Dimension < TestWriter191152Parent
 
       hIn = Marshal::load(Marshal.dump(@@mdHash))
 
-      hReturn = TestWriter191152Parent.run_test(hIn, '19115_2_dimension',
-                                                '//gmd:axisDimensionProperties[1]',
-                                                '//gmd:axisDimensionProperties', 0)
+      hReturn = TestWriter191151Parent.run_test(hIn, '19115_1_dimension',
+                                                '//msr:axisDimensionProperties[1]',
+                                                '//msr:axisDimensionProperties', 0)
 
       assert_equal hReturn[0], hReturn[1]
       assert hReturn[2]
@@ -47,9 +45,9 @@ class TestWriter191152Dimension < TestWriter191152Parent
       hDimension.delete(:dimensionTitle)
       hDimension.delete(:dimensionDescription)
 
-      hReturn = TestWriter191152Parent.run_test(hIn, '19115_2_dimension',
-                                                '//gmd:axisDimensionProperties[2]',
-                                                '//gmd:axisDimensionProperties', 0)
+      hReturn = TestWriter191151Parent.run_test(hIn, '19115_1_dimension',
+                                                '//mrs:axisDimensionProperties[2]',
+                                                '//mrs:axisDimensionProperties', 0)
 
       assert_equal hReturn[0], hReturn[1]
       assert hReturn[2]
