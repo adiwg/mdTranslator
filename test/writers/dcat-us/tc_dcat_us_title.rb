39cf065253b252aaa56aa2c4f1c1691da4f2bb96
Johnathan Aspinwall <jwaspin@gmail.com>
2023-08-30 11:44:16 -0600

    Remove Commented Code

--- test/writers/dcat-us/tc_dcat_us_title.rb
+++ test/writers/dcat-us/tc_dcat_us_title.rb
@@ -22,56 +22,6 @@ class TestWriterDcatUsTitle < TestWriterDcatUsParent
       got = hJsonOut['dcat:title']
 
       assert_equal 'myCitationTitle', got
-
    end
-
-   # def test_alternateTitle
-
-   #    metadata = ADIWG::Mdtranslator.translate(
-   #       file: @@jsonIn, reader: 'mdJson', validate: 'normal',
-   #       writer: 'dcat_us', showAllTags: false)
-
-   #    hJsonOut = JSON.parse(metadata[:writerOutput])
-   #    got = hJsonOut['alternateTitles']
-
-   #    assert_equal 2, got.length
-   #    assert_equal 'alternateTitle0', got[0]
-   #    assert_equal 'alternateTitle1', got[1]
-
-   # end
-
-   # def test_alternateTitle_empty
-
-   #    hJsonIn = JSON.parse(@@jsonIn)
-   #    hJsonIn['metadata']['resourceInfo']['citation']['alternateTitle'] = []
-   #    hIn = hJsonIn.to_json
-
-   #    metadata = ADIWG::Mdtranslator.translate(
-   #       file: hIn, reader: 'mdJson', validate: 'normal',
-   #       writer: 'dcat_us', showAllTags: false)
-
-   #    hJsonOut = JSON.parse(metadata[:writerOutput])
-   #    got = hJsonOut['alternateTitles']
-
-   #    assert_nil got
-
-   # end
-
-   # def test_alternateTitle_missing
-
-   #    hJsonIn = JSON.parse(@@jsonIn)
-   #    hJsonIn['metadata']['resourceInfo']['citation'].delete('alternateTitle')
-   #    hIn = hJsonIn.to_json
-
-   #    metadata = ADIWG::Mdtranslator.translate(
-   #       file: hIn, reader: 'mdJson', validate: 'normal',
-   #       writer: 'dcat_us', showAllTags: false)
-
-   #    hJsonOut = JSON.parse(metadata[:writerOutput])
-   #    got = hJsonOut['alternateTitles']
-
-   #    assert_nil got
-
-   # end
-
+   
 end
