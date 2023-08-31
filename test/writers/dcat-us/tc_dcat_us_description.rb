f4859e3fbc7f6d39c7009b0d3cf3ea9c11568270
Johnathan Aspinwall <jwaspin@gmail.com>
2023-08-31 09:18:59 -0600

    New Description Test With Test File

---
+++ test/writers/dcat-us/tc_dcat_us_description.rb
@@ -0,0 +1,27 @@
+# dcat_us 1 writer tests - title/alternateTitles
+
+# History:
+# 
+
+require 'minitest/autorun'
+require 'json'
+require 'adiwg-mdtranslator'
+require_relative 'dcat_us_test_parent'
+
+class TestWriterDcatUsTitle < TestWriterDcatUsParent
+
+   # get input JSON for test
+   @@jsonIn = TestWriterDcatUsParent.getJson('description.json')
+
+   def test_title
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonIn, reader: 'mdJson', validate: 'normal',
+         writer: 'dcat_us', showAllTags: false)
+
+      hJsonOut = JSON.parse(metadata[:writerOutput])
+      got = hJsonOut['dcat:description']
+
+      assert_equal 'myBody', got
+   end
+   
+end
