4ea3ffabc171780be6947118980c2088514759f7
Johnathan Aspinwall <jwaspin@gmail.com>
2023-09-06 22:14:26 -0600

    Add Unit Tests Access Level
    New tests for public and restricted public.

--- test/writers/dcat_us/tc_dcat_us_access_level.rb
+++ test/writers/dcat_us/tc_dcat_us_access_level.rb
@@ -6,11 +6,39 @@ require_relative 'dcat_us_test_parent'
 class TestWriterDcatUsAccessLevel < TestWriterDcatUsParent
 
    # get input JSON for test
-   @@jsonIn = TestWriterDcatUsParent.getJson('accessLevel.json')
+   @@jsonInPub = TestWriterDcatUsParent.getJson('accessLevel_pub.json')
+   @@jsonInResPub = TestWriterDcatUsParent.getJson('accessLevel_resPub.json')
+   @@jsonInNonPub = TestWriterDcatUsParent.getJson('accessLevel_nonPub.json')
 
-   def test_sample
+   def test_accessLevel_public
       metadata = ADIWG::Mdtranslator.translate(
-         file: @@jsonIn, reader: 'mdJson', validate: 'normal',
+         file: @@jsonInPub, reader: 'mdJson', validate: 'normal',
+         writer: 'dcat_us', showAllTags: false)
+
+      hJsonOut = JSON.parse(metadata[:writerOutput])
+      got = hJsonOut['dcat:accessLevel']
+
+      expect = 'public'
+
+      assert_equal expect, got
+   end
+
+   def test_accessLevel_restricted_public
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonInResPub, reader: 'mdJson', validate: 'normal',
+         writer: 'dcat_us', showAllTags: false)
+
+      hJsonOut = JSON.parse(metadata[:writerOutput])
+      got = hJsonOut['dcat:accessLevel']
+
+      expect = 'restricted public'
+
+      assert_equal expect, got
+   end
+
+   def test_accessLevel_non_public
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonInNonPub, reader: 'mdJson', validate: 'normal',
          writer: 'dcat_us', showAllTags: false)
 
       hJsonOut = JSON.parse(metadata[:writerOutput])
