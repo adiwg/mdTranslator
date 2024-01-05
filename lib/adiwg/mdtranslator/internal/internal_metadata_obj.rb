f0baf071a9c8febab774ced0f1549689f8105681
Johnathan Aspinwall <jwaspin@gmail.com>
2024-01-05 08:51:27 -0700

    Change dateTime to array

--- lib/adiwg/mdtranslator/internal/internal_metadata_obj.rb
+++ lib/adiwg/mdtranslator/internal/internal_metadata_obj.rb
@@ -650,7 +650,7 @@ class InternalMetadata
 
    def newConformanceResult
       {
-         dateTime: nil,
+         dateTime: [],
          scope: {},
          specification: {},
          explanation: nil,
@@ -660,7 +660,7 @@ class InternalMetadata
 
    def newCoverageResult
       {
-         dateTime: nil,
+         dateTime: [],
          scope: {},
          spatialRepresentationType: nil,
          spatialRepresentation: {},
@@ -672,7 +672,7 @@ class InternalMetadata
 
    def newDescriptiveResult
       {
-         dateTime: nil,
+         dateTime: [],
          scope: {},
          statement: nil
       }
@@ -695,7 +695,7 @@ class InternalMetadata
 
    def newQuantitativeResult
       {
-         dateTime: nil,
+         dateTime: [],
          scope: {},
          values: [],
          valueUnits: nil,
