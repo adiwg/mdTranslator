a77e8c6f44e659f71d767fff5bad60a5acefb481
Johnathan Aspinwall <jwaspin@gmail.com>
2024-01-10 18:36:43 -0700

    Update Data Quality Object

--- test/helpers/mdJson_hash_objects.rb
+++ test/helpers/mdJson_hash_objects.rb
@@ -148,6 +148,16 @@ class MdJsonHashWriter
                status: [
                   'status 1'
                ],
+               pointOfContact: [
+                  {
+                     role: 'pointOfContact',
+                     party: [
+                        {
+                           contactId: 'CID001'
+                        }
+                     ]
+                  }
+               ],
                extent: [
                   geographicExtent: [
                      {
@@ -161,16 +171,6 @@ class MdJsonHashWriter
                         }
                      }
                   ]
-               ],
-               pointOfContact: [
-                  {
-                     role: 'pointOfContact',
-                     party: [
-                        {
-                           contactId: 'CID001'
-                        }
-                     ]
-                  }
                ],
                keyword: [
                   {
@@ -245,14 +245,14 @@ class MdJsonHashWriter
    # associated resource ---------------------
    def associatedResource
       {
+         associationType: 'association type',
+         initiativeType: 'initiative type',
          resourceType: [
             {
                type: 'resource type',
                name: 'resource name'
             }
          ],
-         associationType: 'association type',
-         initiativeType: 'initiative type',
          resourceCitation: {
             title: 'citation title',
             date: [
@@ -1054,9 +1054,8 @@ class MdJsonHashWriter
    def dataQuality
       {
          scope: scope,
-         report: [{
-            qualityMeasure: qualityMeasure
-         }]
+         standaloneQualityReport: {},
+         report: []
       }
    end
 
