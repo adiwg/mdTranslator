2e2f185114e4ba38554a37731359911e352ed119
Johnathan Aspinwall <jwaspin@gmail.com>
2024-01-25 16:49:13 -0700

    Fix Result Content
    resultContent -> resultContentDescription
    Uses coverageDescription

--- lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_coverageResult.rb
+++ lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_coverageResult.rb
@@ -3,6 +3,7 @@ require_relative 'mdJson_scope'
 require_relative 'mdJson_spatialRepresentation'
 require_relative 'mdJson_format'
 require_relative 'mdJson_qualityResultFile'
+require_relative 'mdJson_coverageDescription'
 
 module ADIWG
   module Mdtranslator
@@ -16,7 +17,7 @@ module ADIWG
                 json.scope Scope.build(hCoverageResult[:scope])
                 json.spatialRepresentationType hCoverageResult[:spatialRepresentationType]
                 json.spatialRepresentation SpatialRepresentation.build(hCoverageResult[:spatialRepresentation]) unless hCoverageResult[:spatialRepresentation].nil?
-                json.resultContent hCoverageResult[:resultContent]
+                json.resultContentDescription CoverageDescription.build(hCoverageResult[:resultContentDescription]) unless hCoverageResult[:resultContentDescription].nil?
                 json.resourceFormat Format.build(hCoverageResult[:resourceFormat]) unless hCoverageResult[:resourceFormat].nil?
                 json.resultFile QualityResultFile.build(hCoverageResult[:resultFile]) unless hCoverageResult[:resultFile].nil?
             end
