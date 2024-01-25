e461a4abe6f299b455e3b28e0eb52e9f6be7ca58
Jonathan Oliveros <joliveros@xentity.com>
2024-01-25 09:55:06 -0700

    add format and QualityResultFile classes to coverageResult in writer

--- lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_coverageResult.rb
+++ lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_coverageResult.rb
@@ -1,6 +1,8 @@
 require 'jbuilder'
 require_relative 'mdJson_scope'
 require_relative 'mdJson_spatialRepresentation'
+require_relative 'mdJson_format'
+require_relative 'mdJson_qualityResultFile'
 
 module ADIWG
   module Mdtranslator
@@ -15,8 +17,8 @@ module ADIWG
                 json.spatialRepresentationType hCoverageResult[:spatialRepresentationType]
                 json.spatialRepresentation SpatialRepresentation.build(hCoverageResult[:spatialRepresentation]) unless hCoverageResult[:spatialRepresentation].nil?
                 json.resultContent hCoverageResult[:resultContent]
-                json.resourceFormat hCoverageResult[:resourceFormat]
-                json.resultFile hCoverageResult[:resultFile]
+                json.resourceFormat Format.build(hCoverageResult[:resourceFormat]) unless hCoverageResult[:resourceFormat].nil?
+                json.resultFile QualityResultFile.build(hCoverageResult[:resultFile]) unless hCoverageResult[:resultFile].nil?
             end
           end
         end
