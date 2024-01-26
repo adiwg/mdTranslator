34a3f296a4cc79b117646b79e0b1f8586faef305
Johnathan Aspinwall <jwaspin@gmail.com>
2024-01-25 17:01:50 -0700

    Change nil? to empty?

--- lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_coverageResult.rb
+++ lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_coverageResult.rb
@@ -14,12 +14,12 @@ module ADIWG
           def self.build(hCoverageResult)
             Jbuilder.new do |json|
                 json.dateTime hCoverageResult[:dateTime]
-                json.scope Scope.build(hCoverageResult[:scope])
+                json.scope Scope.build(hCoverageResult[:scope]) unless hCoverageResult[:scope].empty?
                 json.spatialRepresentationType hCoverageResult[:spatialRepresentationType]
-                json.spatialRepresentation SpatialRepresentation.build(hCoverageResult[:spatialRepresentation]) unless hCoverageResult[:spatialRepresentation].nil?
-                json.resultContentDescription CoverageDescription.build(hCoverageResult[:resultContentDescription]) unless hCoverageResult[:resultContentDescription].nil?
-                json.resourceFormat Format.build(hCoverageResult[:resourceFormat]) unless hCoverageResult[:resourceFormat].nil?
-                json.resultFile QualityResultFile.build(hCoverageResult[:resultFile]) unless hCoverageResult[:resultFile].nil?
+                json.spatialRepresentation SpatialRepresentation.build(hCoverageResult[:spatialRepresentation]) unless hCoverageResult[:spatialRepresentation].empty?
+                json.resultContentDescription CoverageDescription.build(hCoverageResult[:resultContentDescription]) unless hCoverageResult[:resultContentDescription].empty?
+                json.resourceFormat Format.build(hCoverageResult[:resourceFormat]) unless hCoverageResult[:resourceFormat].empty?
+                json.resultFile QualityResultFile.build(hCoverageResult[:resultFile]) unless hCoverageResult[:resultFile].empty?
             end
           end
         end
