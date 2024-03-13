620f5d000a1f7c5840b5a8355dbc6102542f98c1
Jonathan Oliveros <joliveros@xentity.com>
2024-03-13 10:16:29 -0600

    Add mapping to accessLevel

--- lib/adiwg/mdtranslator/writers/dcat_us/sections/dcat_us_access_level.rb
+++ lib/adiwg/mdtranslator/writers/dcat_us/sections/dcat_us_access_level.rb
@@ -7,15 +7,42 @@ module ADIWG
             module AccessLevel
 
                def self.build(intObj)
+
+                  publicArray = ['public','unclassified', 'unrestricted', 'licenseUnrestricted', 'licenseEndUser']
+                  nonPublicArray = ['Non-public','restricted','confidential','secret','topSecret','forOfficialUseOnly','protected','intellectualPropertyRights','restricted','otherRestrictions','private','statutory','confidential','traditionalKnowledge','personallyIdentifiableInformation']
+                  restrictedPublicArray = ['Restricted public','sensitiveButUnclassified','limitedDistribution','copyright','patent','patentPending','trademark','license','licenseDistributor','in-confidence','threatenedOrEndangered']
+
                   resourceInfo = intObj[:metadata][:resourceInfo]
                   legalConstraints = resourceInfo[:constraints]&.select { |constraint| constraint[:type] == 'legal' }
-                  
-                  accessLevel = legalConstraints&.detect do |constraint|
-                     codes = constraint.dig(:legalConstraint, :accessCodes)
-                     codes&.any? { |code| ["public", "restricted public", "non-public"].include?(code) }
-                  end&.dig(:legalConstraint, :accessCodes)&.find { |code| ["public", "restricted public", "non-public"].include?(code) }
+                  securityConstraints = resourceInfo[:constraints]&.select { |constraint| constraint[:type] == 'security' }
 
-                  accessLevel ? accessLevel : nil
+                  accessLevels = []
+                  accessLevelCodes = []
+                  # Gather codes form security constraints and legal constraints
+                  # return access level that is most restrictive
+                  unless securityConstraints.empty?
+                     securityConstraints.each do |securityConstraint|
+                        code = securityConstraint[:securityConstraint][:classCode]
+                        accessLevelCodes << code                        
+                     end
+                  end
+                  unless legalConstraints.empty?
+                     legalConstraints.each do |legalConstraint|
+                        codes = legalConstraint.dig(:legalConstraint, :accessCodes)
+                        accessLevelCodes << codes
+                     end
+                     
+                  end
+
+                  accessLevelCodes.uniq.each do |code|
+                     if nonPublicArray.include? code
+                        return 'non-public'
+                     elsif restrictedPublicArray.include? code
+                        return 'restricted public'
+                     end
+                  end
+                  
+                  return 'public'
                end           
 
             end
