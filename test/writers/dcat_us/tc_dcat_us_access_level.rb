3cc91d920b8ce610d403ed46ce0fadb517c23c5f
J-Oliveros <97468338+J-Oliveros@users.noreply.github.com>
2024-03-14 10:23:49 -0600

    Add mapping to accessLevel (#352)
    * Add mapping to accessLevel
    
    * Formatting
    
    * Update tests to reflect mapping
    
    * remove unnecessary print statement
    
    * fix legal and security accessLevel codes in tests
    
    * Add test cases for accessLevel.  fix array append to accessLevelCodes.

--- test/writers/dcat_us/tc_dcat_us_access_level.rb
+++ test/writers/dcat_us/tc_dcat_us_access_level.rb
@@ -8,7 +8,11 @@ class TestWriterDcatUsAccessLevel < TestWriterDcatUsParent
    # get input JSON for test
    @@jsonInPub = TestWriterDcatUsParent.getJson('accessLevel_pub.json')
    @@jsonInResPub = TestWriterDcatUsParent.getJson('accessLevel_resPub.json')
+   @@jsonInResPubLegal = TestWriterDcatUsParent.getJson('accessLevel_resPub_legal.json')
+   @@jsonInResPubSecurity = TestWriterDcatUsParent.getJson('accessLevel_resPub_security.json')
    @@jsonInNonPub = TestWriterDcatUsParent.getJson('accessLevel_nonPub.json')
+   @@jsonInNonPubLegal = TestWriterDcatUsParent.getJson('accessLevel_nonPub_legal.json')
+   @@jsonInNonPubSecurity = TestWriterDcatUsParent.getJson('accessLevel_nonPub_security.json')
 
    def test_accessLevel_public
       metadata = ADIWG::Mdtranslator.translate(
@@ -36,9 +40,61 @@ class TestWriterDcatUsAccessLevel < TestWriterDcatUsParent
       assert_equal expect, got
    end
 
+   def test_accessLevel_restricted_public_legal
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonInResPubLegal, reader: 'mdJson', validate: 'normal',
+         writer: 'dcat_us', showAllTags: false)
+
+      hJsonOut = JSON.parse(metadata[:writerOutput])
+      got = hJsonOut['accessLevel']
+
+      expect = 'restricted public'
+
+      assert_equal expect, got
+   end
+
+   def test_accessLevel_restricted_public_security
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonInResPubSecurity, reader: 'mdJson', validate: 'normal',
+         writer: 'dcat_us', showAllTags: false)
+
+      hJsonOut = JSON.parse(metadata[:writerOutput])
+      got = hJsonOut['accessLevel']
+
+      expect = 'restricted public'
+
+      assert_equal expect, got
+   end
+
    def test_accessLevel_non_public
       metadata = ADIWG::Mdtranslator.translate(
          file: @@jsonInNonPub, reader: 'mdJson', validate: 'normal',
+         writer: 'dcat_us', showAllTags: false)
+
+      hJsonOut = JSON.parse(metadata[:writerOutput])
+      got = hJsonOut['accessLevel']
+
+      expect = 'non-public'
+
+      assert_equal expect, got
+   end
+
+   def test_accessLevel_non_public_legal
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonInNonPubLegal, reader: 'mdJson', validate: 'normal',
+         writer: 'dcat_us', showAllTags: false)
+
+      hJsonOut = JSON.parse(metadata[:writerOutput])
+      got = hJsonOut['accessLevel']
+
+      expect = 'non-public'
+
+      assert_equal expect, got
+   end
+
+   def test_accessLevel_non_public_security
+      metadata = ADIWG::Mdtranslator.translate(
+         file: @@jsonInNonPubSecurity, reader: 'mdJson', validate: 'normal',
          writer: 'dcat_us', showAllTags: false)
 
       hJsonOut = JSON.parse(metadata[:writerOutput])
