a79ce2022772258e207cd9f8b8c8128c67010b05
Jonathan Oliveros <joliveros@xentity.com>
2024-03-18 17:49:44 -0600

    Add support to describedByType in DCAT-US writer

--- lib/adiwg/mdtranslator/writers/dcat_us/sections/dcat_us_dcat_us.rb
+++ lib/adiwg/mdtranslator/writers/dcat_us/sections/dcat_us_dcat_us.rb
@@ -18,6 +18,7 @@ require_relative 'dcat_us_references'
 require_relative 'dcat_us_landing_page'
 require_relative 'dcat_us_system_of_records'
 require_relative 'dcat_us_description'
+require_relative 'dcat_us_described_by_type'
 
 module ADIWG
    module Mdtranslator
@@ -49,6 +50,7 @@ module ADIWG
                references = References.build(intObj)
                landingPage = LandingPage.build(intObj)
                systemOfRecords = SystemOfRecords.build(intObj)
+               describedByType = DescribedByType.build(intObj)
 
                @Namespace = ADIWG::Mdtranslator::Writers::Dcat_us
 
@@ -82,7 +84,7 @@ module ADIWG
                   json.set!('systemOfRecords', systemOfRecords)
                   # json.set!('primaryITInvestmentUII', metadataInfo[:metadataId])
                   json.set!('describedBy', describedBy)
-                  # json.set!('describedByType', metadataInfo[:metadataOnlineOptions][0][:olResProtocol])
+                  json.set!('describedByType', describedByType)
                   # json.set!('conformsTo', metadataInfo[:metadataStandards][0][:standardName])
                end
             end
--- lib/adiwg/mdtranslator/writers/dcat_us/sections/dcat_us_described_by_type.rb
+++ lib/adiwg/mdtranslator/writers/dcat_us/sections/dcat_us_described_by_type.rb
@@ -4,24 +4,28 @@ module ADIWG
    module Mdtranslator
       module Writers
          module Dcat_us
-            module DescribedBy
+            module DescribedByType
 
                def self.build(intObj)
+
+                  #  metadataInfo[:metadataOnlineOptions][0][:olResProtocol]
                   dataDictionaries = intObj[:dataDictionaries]
+                  describedByType = ''
                   dataDictionaries.each do |dataDictionary|
                      unless dataDictionary[:includedWithDataset]
                         onlineResources = dataDictionary[:citation][:onlineResources]
                         onlineResources.each do |resource|
-                           if resource[:olResURI]
-                              return resource[:olResURI]
+                           if resource[:olResURI] && !resource[:olResURI].end_with?('.html')
+                              describedByType = resource[:olResProtocol]
+                              break
                            end
                         end
                      end
                   end
                 
-                  nil
-                end                
-                
+                  describedByType
+               end                
+
             end
          end
       end
