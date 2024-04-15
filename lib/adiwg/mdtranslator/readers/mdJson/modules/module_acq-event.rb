f53540f622b6ad278c3cd3e49276994274f75a08
Jonathan Oliveros <joliveros@xentity.com>
2024-04-15 11:27:07 -0600

    add acq-event class

---
+++ lib/adiwg/mdtranslator/readers/mdJson/modules/module_acq-event.rb
@@ -0,0 +1,15 @@
+module ADIWG
+    module Mdtranslator
+        module Readers
+            module MdJson
+  
+                module AcqEvent
+                    def self.unpack(hAcqEvent, responseObj, inContext = nil)
+                        
+                    end
+                end
+
+            end
+        end
+    end
+end
