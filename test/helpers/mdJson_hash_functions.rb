cf39d3377b803024fde2b246a92ea6106a79e147
Johnathan Aspinwall <jwaspin@gmail.com>
2024-04-30 11:23:24 -0600

    chore: Update adiwg-mdjson_schemas and adiwg-mdcodes gem dependencies to latest pre-beta versions

--- test/helpers/mdJson_hash_functions.rb
+++ test/helpers/mdJson_hash_functions.rb
@@ -85,7 +85,7 @@ class MdJsonHashWriter
                description: 'description'
             },
             evaluationMethod: {
-               type: 'DQ_EvaluationMethod',
+               type: 'evaluationMethod',
                dateTime: ['2019-06-11T08:35:00', '2019-06-11T09:00:00'],
                methodDescription: 'evaluation method description',
                evaluationProcedure: { title: 'EP Title' },
