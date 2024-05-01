d4be8d70d865df6ae34fc15fb81f72f6785d4416
Jonathan Oliveros <joliveros@xentity.com>
2024-05-01 11:26:15 -0600

    uncomment necessary code

--- lib/adiwg/mdtranslator/readers/mdJson/modules/module_acquisition.rb
+++ lib/adiwg/mdtranslator/readers/mdJson/modules/module_acquisition.rb
@@ -26,84 +26,84 @@ module ADIWG
                   intMetadataClass = InternalMetadata.new
                   intAcquisition = intMetadataClass.newAcquisition
  
-                  # if hAcquisition.has_key?('scope')
-                  #    unless hAcquisition['scope'].empty?
-                  #       hReturn = Scope.unpack(hAcquisition['scope'], responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:scope] = hReturn
-                  #       end
-                  #    end
-                  # end
+                  if hAcquisition.has_key?('scope')
+                     unless hAcquisition['scope'].empty?
+                        hReturn = Scope.unpack(hAcquisition['scope'], responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:scope] = hReturn
+                        end
+                     end
+                  end
 
-                  # if hAcquisition.has_key?('plan')
-                  #    aItems = hAcquisition['plan']
-                  #    aItems.each do |item|
-                  #       hReturn = AcqPlan.unpack(item, responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:plans] << hReturn
-                  #       end
-                  #    end
-                  # end
+                  if hAcquisition.has_key?('plan')
+                     aItems = hAcquisition['plan']
+                     aItems.each do |item|
+                        hReturn = AcqPlan.unpack(item, responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:plans] << hReturn
+                        end
+                     end
+                  end
 
-                  # if hAcquisition.has_key?('requirement')
-                  #    aItems = hAcquisition['requirement']
-                  #    aItems.each do |item|
-                  #       hReturn = AcqRequirement.unpack(item, responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:requirements] << hReturn
-                  #       end
-                  #    end
-                  # end
+                  if hAcquisition.has_key?('requirement')
+                     aItems = hAcquisition['requirement']
+                     aItems.each do |item|
+                        hReturn = AcqRequirement.unpack(item, responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:requirements] << hReturn
+                        end
+                     end
+                  end
 
-                  # if hAcquisition.has_key?('objective')
-                  #    aItems = hAcquisition['objective']
-                  #    aItems.each do |item|
-                  #       hReturn = AcqObjective.unpack(item, responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:objectives] << hReturn
-                  #       end
-                  #    end
-                  # end
+                  if hAcquisition.has_key?('objective')
+                     aItems = hAcquisition['objective']
+                     aItems.each do |item|
+                        hReturn = AcqObjective.unpack(item, responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:objectives] << hReturn
+                        end
+                     end
+                  end
 
-                  # if hAcquisition.has_key?('platform')
-                  #    aItems = hAcquisition['platform']
-                  #    aItems.each do |item|
-                  #       hReturn = AcqPlatform.unpack(item, responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:platforms] << hReturn
-                  #       end
-                  #    end
-                  # end
+                  if hAcquisition.has_key?('platform')
+                     aItems = hAcquisition['platform']
+                     aItems.each do |item|
+                        hReturn = AcqPlatform.unpack(item, responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:platforms] << hReturn
+                        end
+                     end
+                  end
 
-                  # if hAcquisition.has_key?('instrument')
-                  #    aItems = hAcquisition['instrument']
-                  #    aItems.each do |item|
-                  #       hReturn = AcqInstrument.unpack(item, responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:instruments] << hReturn
-                  #       end
-                  #    end
-                  # end
+                  if hAcquisition.has_key?('instrument')
+                     aItems = hAcquisition['instrument']
+                     aItems.each do |item|
+                        hReturn = AcqInstrument.unpack(item, responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:instruments] << hReturn
+                        end
+                     end
+                  end
 
-                  # if hAcquisition.has_key?('operation')
-                  #    aItems = hAcquisition['operation']
-                  #    aItems.each do |item|
-                  #       hReturn = AcqOperation.unpack(item, responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:operations] << hReturn
-                  #       end
-                  #    end
-                  # end
+                  if hAcquisition.has_key?('operation')
+                     aItems = hAcquisition['operation']
+                     aItems.each do |item|
+                        hReturn = AcqOperation.unpack(item, responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:operations] << hReturn
+                        end
+                     end
+                  end
 
-                  # if hAcquisition.has_key?('event')
-                  #    aItems = hAcquisition['event']
-                  #    aItems.each do |item|
-                  #       hReturn = AcqEvent.unpack(item, responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:events] << hReturn
-                  #       end
-                  #    end
-                  # end
+                  if hAcquisition.has_key?('event')
+                     aItems = hAcquisition['event']
+                     aItems.each do |item|
+                        hReturn = AcqEvent.unpack(item, responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:events] << hReturn
+                        end
+                     end
+                  end
 
                   if hAcquisition.has_key?('pass')
                      aItems = hAcquisition['pass']
@@ -115,16 +115,16 @@ module ADIWG
                      end
                   end
                   
-                  # if hAcquisition.has_key?('environment')
-                  #    aItems = hAcquisition['environment']
-                  #    aItems.each do |item|
-                  #       hReturn = AcqEnvironment.unpack(item, responseObj, outContext)
-                  #       unless hReturn.nil?
-                  #          intAcquisition[:environments] << hReturn
-                  #       end
-                  #    end
+                  if hAcquisition.has_key?('environment')
+                     aItems = hAcquisition['environment']
+                     aItems.each do |item|
+                        hReturn = AcqEnvironment.unpack(item, responseObj, outContext)
+                        unless hReturn.nil?
+                           intAcquisition[:environments] << hReturn
+                        end
+                     end
 
-                  # end
+                  end
 
                   return intAcquisition
 
