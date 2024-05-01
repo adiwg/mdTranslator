6377d21437907f3ec1b802cfc35af12c6c206d5e
Jonathan Oliveros <joliveros@xentity.com>
2024-05-01 15:22:10 -0600

    Change to validate status and planType using mdCodes. fix array iteration for planOperations and satisfiedRequirements.

--- lib/adiwg/mdtranslator/readers/mdJson/modules/module_plan.rb
+++ lib/adiwg/mdtranslator/readers/mdJson/modules/module_plan.rb
@@ -1,4 +1,7 @@
+require 'adiwg/mdtranslator/internal/module_codelistFun'
 require_relative 'module_citation'
+require_relative 'module_operation'
+require_relative 'module_requirement'
 
 module ADIWG
     module Mdtranslator
@@ -16,66 +19,58 @@ module ADIWG
                             return nil
                         end
 
+                        intMetadataClass = InternalMetadata.new
                         intPlan = intMetadataClass.newPlan
 
-                        statusArray = [
-                            "accepted",
-                            "cancelled",
-                            "completed",
-                            "deprecated",
-                            "final",
-                            "funded",
-                            "historicalArchive",
-                            "notAccepted",
-                            "obsolete",
-                            "onGoing",
-                            "pending",
-                            "planned",
-                            "proposed",
-                            "required",
-                            "retired",
-                            "superseded",
-                            "suspended",
-                            "tentative",
-                            "underDevelopment",
-                            "valid",
-                            "withdrawn"
-                        ]
+                        outContext = 'plan'
+                        outContext = inContext + ' > ' + outContext unless inContext.nil?
 
-                        if hPlan.has_key('planId')
+                        if hPlan.has_key?('planId')
                             intPlan[:planId] = hPlan['planId']
                             
                         else
-                            @MessagePath.issueError(42, responseObj, inContext)
+                            @MessagePath.issueError(42, responseObj, outContext)
                         end
 
-                        if hPlan.has_key('planType')
+                        if hPlan.has_key?('planType') && CodelistFun.validateItem('iso_geometryTypeCode', hPlan['planType'])
                             intPlan[:planType] = hPlan['planType']
                         end
 
-                        if hPlan.has_key('status') && statusArray.include?(hPlan['status'])
+                        if hPlan.has_key?('status') && CodelistFun.validateItem('iso_progress', hPlan['status'])
                             intPlan[:status] = hPlan['status']
                         else
-                            @MessagePath.issueError(43, responseObj, inContext)
+                            @MessagePath.issueError(43, responseObj, outContext)
                         end
 
-                        if hPlan.has_key('citation')
-                            hReturn = Citation.unpack(hPlan['citation'], responseObj, inContext)
+                        if hPlan.has_key?('citation')
+                            hReturn = Citation.unpack(hPlan['citation'], responseObj, outContext)
                             unless hReturn.nil?
                                 intPlan[:citation] = hReturn
                             end
                         else
-                            @MessagePath.issueError(44, responseObj, inContext)
+                            @MessagePath.issueError(44, responseObj, outContext)
                         end
 
                         if hPlan.has_key?('planOperations')
-                            intPlan[:planOperations] = hPlan['planOperations']
-                            
+                            hPlan['planOperations'].each do |item|
+                                hReturn = Operation.unpack(item, responseObj, outContext)
+                                unless hReturn.nil?
+                                    intPlan[:planOperations] << hReturn
+                                end
+                            end                            
                         end
 
                         if hPlan.has_key?('satisfiedRequirements')
-                            intPlan[:satisfiedRequirements] = hPlan['satisfiedRequirements']
+                            hPlan['satisfiedRequirements'].each do |item|
+                                hReturn = Requirement.unpack(item, responseObj, outContext)
+                                unless hReturn.nil?
+                                    intPlan[:satisfiedRequirements] << hReturn
+                                end
+                            end
                         end
+
+                        puts intPlan
+                        intPlan
 
                     end
                 end
