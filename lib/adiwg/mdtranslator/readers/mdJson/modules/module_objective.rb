d8b14903edbe3c93718350133a2edde893267bb8
Jonathan Oliveros <joliveros@xentity.com>
2024-05-01 16:08:27 -0600

    add relation to codelist found in mdCodes for objectiveType. Update class to handle all fields.

--- lib/adiwg/mdtranslator/readers/mdJson/modules/module_objective.rb
+++ lib/adiwg/mdtranslator/readers/mdJson/modules/module_objective.rb
@@ -1,4 +1,9 @@
+require 'adiwg/mdtranslator/internal/module_codelistFun'
 require_relative 'module_identifier'
+require_relative 'module_extent'
+require_relative 'module_event'
+require_relative 'module_pass'
+require_relative 'module_instrument'
 
 module ADIWG
     module Mdtranslator
@@ -8,20 +13,18 @@ module ADIWG
                 module Objective
                     def self.unpack(hObjective, responseObj, inContext = nil)
 
+                        @MessagePath = ADIWG::Mdtranslator::Readers::MdJson::MdJson
+
                         intMetadataClass = InternalMetadata.new
                         intObjective = intMetadataClass.newObjective
 
                         outContext = 'objective'
                         outContext = inContext + ' > ' + outContext unless inContext.nil?
 
-                        objectiveTypeArray = [
-                            "instantaneousCollection",
-                            "persistentView",
-                            "survey"
-                        ]
-
-                        if hObjective.has_key?('ObjectiveId')
-                            intObjective[:ObjectiveId] = hAcqPlan['requirementId']
+                        if hObjective.has_key?('objectiveId')
+                            intObjective[:objectiveId] = hObjective['objectiveId']
+                        else
+                            @MessagePath.issueError(200, responseObj, outContext)
                         end
 
                         if hObjective.has_key?('identifier')
@@ -30,6 +33,8 @@ module ADIWG
                                 hReturn = Identifier.unpack(item, responseObj, outContext)
                                 unless hReturn.nil?
                                     intObjective[:identifiers] << hReturn
+                                else
+                                    @MessagePath.issueError(201, responseObj, outContext)
                                 end
                             end
                         end
@@ -41,14 +46,52 @@ module ADIWG
                         if hObjective.has_key?('objectiveType')
                             aItems = hObjective['objectiveType']
                             aItems.each do |item|
-                                if objectiveTypeArray.include?(item)
+                                if CodelistFun.validateItem('iso_objectiveTypeCode', item)
                                     intObjective[:objectiveTypes] << item
                                 end
                             end
                         end
 
                         if hObjective.has_key?('function')
-                            intObjective[:function] = hObjective['function']
+                            hObjective['function'].each do |item|
+                                intObjective[:functions] << item
+                            end
+                        end
+
+                        if hObjective.has_key?('extent')
+                            hObjective['extent'].each do |item|
+                                hReturn = Extent.unpack(item, responseObj, outContext)
+                                unless hReturn.nil?
+                                    intObjective[:extents] << hReturn
+                                end
+                            end
+                        end
+
+                        if hObjective.has_key?('objectiveOccurrence')
+                            hObjective['objectiveOccurrence'].each do |item|
+                                hReturn = Event.unpack(item, responseObj, outContext)
+                                unless hReturn.nil?
+                                    intObjective[:occurrences] << hReturn
+                                end
+                            end
+                        end
+
+                        if hObjective.has_key?('pass')
+                            hObjective['pass'].each do |item|
+                                hReturn = Pass.unpack(item, responseObj, outContext)
+                                unless hReturn.nil?
+                                    intObjective[:passes] << hReturn
+                                end
+                            end
+                        end
+
+                        if hObjective.has_key?('sensingInstrument')
+                            hObjective['sensingInstrument'].each do |item|
+                                hReturn = Instrument.unpack(item, responseObj, outContext)
+                                unless hReturn.nil?
+                                    intObjective[:sensingInstruments] << hReturn
+                                end
+                            end
                         end
 
                         intObjective
