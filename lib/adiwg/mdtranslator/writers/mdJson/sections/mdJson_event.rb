925fd0fb21818a62edf7023fbf574161205641e2
Jonathan Oliveros <joliveros@xentity.com>
2024-05-02 16:04:26 -0600

    Add Event class to writer.

---
+++ lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_event.rb
@@ -0,0 +1,38 @@
+require 'jbuilder'
+require_relative 'mdJson_identifier'
+require_relative 'mdJson_dateTime'
+# require_relative 'mdJson_objective'
+# require_relative 'mdJson_pass'
+# require_relative 'mdJson_instrument'
+
+
+module ADIWG
+    module Mdtranslator
+        module Writers
+            module MdJson
+
+                module Event
+
+                    @Namespace = ADIWG::Mdtranslator::Writers::MdJson
+
+                    def self.build(hEvent)
+                        puts hEvent
+
+                        Jbuilder.new do |json|
+                            json.eventId hEvent[:eventId]
+                            json.identifiers Identifier.build(hEvent[:identifier])
+                            json.trigger hEvent[:eventTrigger]
+                            json.context hEvent[:eventContext]
+                            json.sequence hEvent[:sequence]
+                            json.time DateTime.build(hEvent[:time])
+                            # json.expectedObjective @Namespace.json_map(hEvent[:expectedObjectives], Objective)
+                            # json.relatedPass Pass.build(hEvent[:relatedPass])
+                            # json.relatedSensor @Namespace.json_map(hEvent[:Instruments], Instrument)
+                        end
+
+                    end 
+                end # Event
+            end
+        end
+    end
+end
