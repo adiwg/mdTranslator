1a7daf874b3f9d655c5fe8576347365a07fd8555
Jonathan Oliveros <joliveros@xentity.com>
2024-05-08 10:44:44 -0600

    fix keys for Event class writer.

--- lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_event.rb
+++ lib/adiwg/mdtranslator/writers/mdJson/sections/mdJson_event.rb
@@ -1,9 +1,9 @@
 require 'jbuilder'
 require_relative 'mdJson_identifier'
 require_relative 'mdJson_dateTime'
-# require_relative 'mdJson_objective'
-# require_relative 'mdJson_pass'
-# require_relative 'mdJson_instrument'
+require_relative 'mdJson_objective'
+require_relative 'mdJson_pass'
+require_relative 'mdJson_instrument'
 
 
 module ADIWG
@@ -16,17 +16,17 @@ module ADIWG
                     @Namespace = ADIWG::Mdtranslator::Writers::MdJson
 
                     def self.build(hEvent)
-
+                        puts hEvent
                         Jbuilder.new do |json|
                             json.eventId hEvent[:eventId]
-                            json.identifiers Identifier.build(hEvent[:identifier])
-                            json.trigger hEvent[:eventTrigger]
-                            json.context hEvent[:eventContext]
+                            json.identifier Identifier.build(hEvent[:identifier])
+                            json.trigger hEvent[:trigger]
+                            json.context hEvent[:context]
                             json.sequence hEvent[:sequence]
                             json.time DateTime.build(hEvent[:time])
-                            # json.expectedObjective @Namespace.json_map(hEvent[:expectedObjectives], Objective)
-                            # json.relatedPass Pass.build(hEvent[:relatedPass])
-                            # json.relatedSensor @Namespace.json_map(hEvent[:relatedSensors], Instrument)
+                            json.expectedObjective @Namespace.json_map(hEvent[:expectedObjectives], Objective)
+                            json.relatedPass Pass.build(hEvent[:relatedPass])
+                            json.relatedSensor @Namespace.json_map(hEvent[:relatedSensors], Instrument)
                         end
 
                     end 
