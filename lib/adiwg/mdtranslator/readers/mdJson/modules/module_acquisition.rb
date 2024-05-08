295166c421879ce8c647488f517d45e511caa459
Jonathan Oliveros <joliveros@xentity.com>
2024-05-08 11:20:41 -0600

    Fix Environment class for use in Acquisition

--- lib/adiwg/mdtranslator/readers/mdJson/modules/module_acquisition.rb
+++ lib/adiwg/mdtranslator/readers/mdJson/modules/module_acquisition.rb
@@ -117,7 +117,7 @@ module ADIWG
                   end
                   
                   if hAcquisition.has_key?('environment')
-                     intAcquisition[:environments] = hAcquisition['environment']
+                     intAcquisition[:environments] = Environment.unpack(hAcquisition['environment'], responseObj, outContext)
                   end
                   
                   return intAcquisition
--- lib/adiwg/mdtranslator/readers/mdJson/modules/module_environment.rb
+++ lib/adiwg/mdtranslator/readers/mdJson/modules/module_environment.rb
@@ -5,32 +5,34 @@ module ADIWG
   
                 module Environment
                     def self.unpack(hEnvironment, responseObj, inContext = nil)
+
+                        intMetadataClass = InternalMetadata.new
                         intEnvironment = intMetadataClass.newEnvironment
 
                         outContext = 'environment'
                         outContext = inContext + ' > ' + outContext unless inContext.nil?
 
-                        if hEnvironment.has_key('averageAirTemperature')
+                        if hEnvironment.has_key?('averageAirTemperature')
                             intEnvironment[:averageAirTemperature] = hEnvironment['averageAirTemperature']
                         end
 
-                        if hEnvironment.has_key('maxRelativeHumidity')
+                        if hEnvironment.has_key?('maxRelativeHumidity')
                             intEnvironment[:maxRelativeHumidity] = hEnvironment['maxRelativeHumidity']
                         end
 
-                        if hEnvironment.has_key('maxAltitude')
+                        if hEnvironment.has_key?('maxAltitude')
                             intEnvironment[:maxAltitude] = hEnvironment['maxAltitude']
                         end
 
-                        if hEnvironment.has_key('meteorologicalConditions')
+                        if hEnvironment.has_key?('meteorologicalConditions')
                             intEnvironment[:meteorologicalConditions] = hEnvironment['meteorologicalConditions']
                         end
 
-                        if hEnvironment.has_key('solarAzimuth')
+                        if hEnvironment.has_key?('solarAzimuth')
                             intEnvironment[:solarAzimuth] = hEnvironment['solarAzimuth']
                         end
 
-                        if hEnvironment.has_key('solarElevation')
+                        if hEnvironment.has_key?('solarElevation')
                             intEnvironment[:solarElevation] = hEnvironment['solarElevation']
                         end
 
