a4b260740a4ae1b0d5d2b9e3316e89d35d8ca1a6
Johnathan Aspinwall <jwaspin@gmail.com>
2025-02-10 21:28:06 -0700

    Remove redundant HTML details section for data quality in mdtranslator

--- lib/adiwg/mdtranslator/writers/html/sections/html_body.rb
+++ lib/adiwg/mdtranslator/writers/html/sections/html_body.rb
@@ -178,10 +178,6 @@ module ADIWG
                                     @html.summary('Quality', {'class' => 'h3'})
                                     dataQualityClass.writeHtml(hDataQuality)
                                  end
-                                 @html.details do
-                                    @html.summary('Quality', {'class' => 'h3'})
-                                    dataQualityClass.writeHtml(hDataQuality)
-                                 end
                               end
                            end
                         end
